97ad37adb15fc1dea651123ded55eb0097c3d9d8
Kam D Kasravi <kam.d.kasravi@intel.com>
2019-04-29 07:30:00 -0700

    keep generation and testing within this repo

--- hack/gen-test-targets.sh
+++ hack/gen-test-targets.sh
@@ -6,17 +6,10 @@ if [[ $(basename $PWD) != "manifests" ]]; then
   echo "must be at manifests root directory to run $0"
   exit 1
 fi
-rm -f tests/*
+rm -f $(ls tests/*_test.go | grep -v kusttestharness_test.go)
 for i in $(find * -type d -exec sh -c '(ls -p "{}"|grep />/dev/null)||echo "{}"' \; | egrep -v 'tests|hack'); do
   absdir=$(pwd)/$i
   testname=$(get-target-name $absdir)_test.go
   echo generating $testname from $absdir
   ./hack/gen-test-target.sh $absdir > tests/$testname
 done
-if [[ -d $GOPATH/src/github.com/kubeflow/kubeflow/bootstrap/v2/pkg/kfapp/kustomize ]]; then
-  for i in $(ls tests/*_test.go|grep -v kusttestharness_test.go);do
-    test=$(basename $i)
-    rm $GOPATH/src/github.com/kubeflow/kubeflow/bootstrap/v2/pkg/kfapp/kustomize/$test
-  done
-  mv tests/* $GOPATH/src/github.com/kubeflow/kubeflow/bootstrap/v2/pkg/kfapp/kustomize
-fi
