373af360f3a8e21620c960bd7a7292da4e810c59
Kam D Kasravi <kam.d.kasravi@intel.com>
2019-04-29 09:19:09 -0700

    added script to gen target tree in README.md

--- hack/utils.sh
+++ hack/utils.sh
@@ -1,3 +1,14 @@
+manifests-tree()
+{
+   find * -type d  | egrep -v 'tests|hack' | tree --fromfile -C | sed 's/─ base/🎯base/g' | sed 's/─ gcp/🎯gcp/g' | sed 's/─ \(cluster*\)/─ 🎯\1/g' | sed 's/─ \(namespaced*\)/─ 🎯\1/g'
+#  find * -type d  | egrep -v 'tests|hack' | awk '!/\.$/ { \
+#    for (i=1; i<NF; i++) { \
+#        printf("%12s", "╬            ") \
+#    } \
+#    print "╬⊳🗳  "$NF"🎯"  \
+#}' FS='/'
+}
+
 get-target()
 {
   local b=$(basename $1)
