e3f6c29e570555ee1fb430e2a6cabda0c325a2f9
Kam Kasravi <kam.d.kasravi@intel.com>
2019-05-09 15:59:58 -0700

    test_harness w/ fixes for the unit tests to pass (#50)
    * test_harness w/ fixes for the unit tests to pass
    
    * update for README.md
    
    * update to README.md
    
    * added test harness files to generate unit tests

---
+++ hack/gen-test-targets.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+# gen-test-targets will generate units tests under tests/ for all directories that 
+# have a kustomization.yaml. This script first finds all directories and then calls
+# gen-test-target to generate each golang unit test.
+# The script is based on kusttestharness_test.go from kubernetes-sigs/kustomize/pkg/target
+#
+if [[ $(basename $PWD) != "manifests" ]]; then
+  echo "must be at manifests root directory to run $0"
+  exit 1
+fi
+
+source hack/utils.sh
+rm -f $(ls tests/*_test.go | grep -v kusttestharness_test.go)
+for i in $(find * -type d -exec sh -c '(ls -p "{}"|grep />/dev/null)||echo "{}"' \; | egrep -v 'docs|tests|hack'); do
+  absdir=$(pwd)/$i
+  testname=$(get-target-name $absdir)_test.go
+  echo generating $testname from $absdir
+  ./hack/gen-test-target.sh $absdir > tests/$testname
+done
