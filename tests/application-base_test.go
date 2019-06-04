67949b904753c91ec38588487db4437731a5adf9
Kam Kasravi <kam.d.kasravi@intel.com>
2019-06-04 14:17:52 -0700

    add application-controller and application overlays (disabled by default) (#67)
    * add application-controller and application overlays
    
    * fix #21
    
    * snapshot
    
    * rebase from upstream/master
    
    * cannot merge nameprefix, namespace, commonlabels in mixin overlays
    
    * no generator until next version of kustomize
    
    * allow commonlabel to be promoted
    
    * fix for kustomize bug in stateful-set.yaml
    
    * errors in statefulset and application
    
    * remove namesuffix for configmap
    
    * fix for tfjobs crd
    
    * regen unit tests
    
    * fix for image
    
    * fixes to application statefulset.yaml
    
    * fix for application, tests regen
    
    * give application-controller the right RBAC role

--- tests/application-base_test.go
+++ tests/application-base_test.go
@@ -12,24 +12,335 @@ import (
 )
 
 func writeApplicationBase(th *KustTestHarness) {
-	th.writeF("/manifests/application/base/application.yaml", `
-apiVersion: app.k8s.io/v1beta1
-kind: Application
+	th.writeF("/manifests/application/base/crd.yaml", `
+apiVersion: apiextensions.k8s.io/v1beta1
+kind: CustomResourceDefinition
 metadata:
-  name: kubeflow
+  creationTimestamp: null
+  name: applications.app.k8s.io
 spec:
-  componentKinds: []
-  assemblyPhase: "Pending"
-  descriptor:
-    type: "kubeflow"
+  group: app.k8s.io
+  names:
+    kind: Application
+    plural: applications
+  scope: Namespaced
+  validation:
+    openAPIV3Schema:
+      properties:
+        apiVersion:
+          type: string
+        kind:
+          type: string
+        metadata:
+          type: object
+        spec:
+          properties:
+            addOwnerRef:
+              type: boolean
+            assemblyPhase:
+              type: string
+            componentKinds:
+              items:
+                type: object
+              type: array
+            descriptor:
+              properties:
+                description:
+                  type: string
+                icons:
+                  items:
+                    properties:
+                      size:
+                        type: string
+                      src:
+                        type: string
+                      type:
+                        type: string
+                    required:
+                    - src
+                    type: object
+                  type: array
+                keywords:
+                  items:
+                    type: string
+                  type: array
+                links:
+                  items:
+                    properties:
+                      description:
+                        type: string
+                      url:
+                        type: string
+                    type: object
+                  type: array
+                maintainers:
+                  items:
+                    properties:
+                      email:
+                        type: string
+                      name:
+                        type: string
+                      url:
+                        type: string
+                    type: object
+                  type: array
+                notes:
+                  type: string
+                owners:
+                  items:
+                    properties:
+                      email:
+                        type: string
+                      name:
+                        type: string
+                      url:
+                        type: string
+                    type: object
+                  type: array
+                type:
+                  type: string
+                version:
+                  type: string
+              type: object
+            info:
+              items:
+                properties:
+                  name:
+                    type: string
+                  type:
+                    type: string
+                  value:
+                    type: string
+                  valueFrom:
+                    properties:
+                      configMapKeyRef:
+                        properties:
+                          apiVersion:
+                            type: string
+                          fieldPath:
+                            type: string
+                          key:
+                            type: string
+                          kind:
+                            type: string
+                          name:
+                            type: string
+                          namespace:
+                            type: string
+                          resourceVersion:
+                            type: string
+                          uid:
+                            type: string
+                        type: object
+                      ingressRef:
+                        properties:
+                          apiVersion:
+                            type: string
+                          fieldPath:
+                            type: string
+                          host:
+                            type: string
+                          kind:
+                            type: string
+                          name:
+                            type: string
+                          namespace:
+                            type: string
+                          path:
+                            type: string
+                          resourceVersion:
+                            type: string
+                          uid:
+                            type: string
+                        type: object
+                      secretKeyRef:
+                        properties:
+                          apiVersion:
+                            type: string
+                          fieldPath:
+                            type: string
+                          key:
+                            type: string
+                          kind:
+                            type: string
+                          name:
+                            type: string
+                          namespace:
+                            type: string
+                          resourceVersion:
+                            type: string
+                          uid:
+                            type: string
+                        type: object
+                      serviceRef:
+                        properties:
+                          apiVersion:
+                            type: string
+                          fieldPath:
+                            type: string
+                          kind:
+                            type: string
+                          name:
+                            type: string
+                          namespace:
+                            type: string
+                          path:
+                            type: string
+                          port:
+                            format: int32
+                            type: integer
+                          resourceVersion:
+                            type: string
+                          uid:
+                            type: string
+                        type: object
+                      type:
+                        type: string
+                    type: object
+                type: object
+              type: array
+            selector:
+              type: object
+          type: object
+        status:
+          properties:
+            components:
+              items:
+                properties:
+                  group:
+                    type: string
+                  kind:
+                    type: string
+                  link:
+                    type: string
+                  name:
+                    type: string
+                  status:
+                    type: string
+                type: object
+              type: array
+            conditions:
+              items:
+                properties:
+                  lastTransitionTime:
+                    format: date-time
+                    type: string
+                  lastUpdateTime:
+                    format: date-time
+                    type: string
+                  message:
+                    type: string
+                  reason:
+                    type: string
+                  status:
+                    type: string
+                  type:
+                    type: string
+                required:
+                - type
+                - status
+                type: object
+              type: array
+            observedGeneration:
+              format: int64
+              type: integer
+          type: object
+  version: v1beta1
+status:
+  acceptedNames:
+    kind: ""
+    plural: ""
+  conditions: []
+  storedVersions: []
+`)
+	th.writeF("/manifests/application/base/cluster-role.yaml", `
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRole
+metadata:
+  name: controller-cluster-role
+rules:
+- apiGroups:
+  - '*'
+  resources:
+  - '*'
+  verbs:
+  - get
+  - list
+  - update
+  - patch
+  - watch
+- apiGroups:
+  - app.k8s.io
+  resources:
+  - '*'
+  verbs:
+  - '*'
+`)
+	th.writeF("/manifests/application/base/cluster-role-binding.yaml", `
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: controller-cluster-role-binding
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: controller-cluster-role
+subjects:
+- kind: ServiceAccount
+  name: controller-service-account
+`)
+	th.writeF("/manifests/application/base/service-account.yaml", `
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: controller-service-account
+`)
+	th.writeF("/manifests/application/base/service.yaml", `
+apiVersion: v1
+kind: Service
+metadata:
+  name: controller-service
+spec:
+  ports:
+  - port: 443
+`)
+	th.writeF("/manifests/application/base/stateful-set.yaml", `
+apiVersion: apps/v1
+kind: StatefulSet
+metadata:
+  name: stateful-set
+spec:
+  serviceName: controller-service
+  template:
+    spec:
+      containers:
+      - name: manager
+        command:
+        - /root/manager
+        image: controller:latest
+        imagePullPolicy: Always
+        env:
+          - name: POD_NAMESPACE
+            valueFrom:
+              fieldRef:
+                fieldPath: metadata.namespace
+      serviceAccountName: controller-service-account
+  volumeClaimTemplates: []
 `)
 	th.writeK("/manifests/application/base", `
 apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
 resources:
-- application.yaml
+- crd.yaml
+- cluster-role.yaml
+- cluster-role-binding.yaml
+- service-account.yaml
+- service.yaml
+- stateful-set.yaml
+namespace: kubeflow
+nameprefix: application-controller-
 commonLabels:
-  kustomize.component: application
+  kustomize-component: application-controller
 `)
 }
 
