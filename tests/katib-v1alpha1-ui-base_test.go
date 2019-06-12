72eebd7f89af0d0ead7b51c5fd47bd7527d456f6
Richard Liu <39319471+richardsliu@users.noreply.github.com>
2019-06-12 13:50:23 -0700

    Reorganize katib v1alpha1 packages (#128)
    * Reorganize katib v1alpha1 packages
    
    * Fix tests

---
+++ tests/katib-v1alpha1-ui-base_test.go
@@ -0,0 +1,151 @@
+package tests_test
+
+import (
+	"sigs.k8s.io/kustomize/k8sdeps/kunstruct"
+	"sigs.k8s.io/kustomize/k8sdeps/transformer"
+	"sigs.k8s.io/kustomize/pkg/fs"
+	"sigs.k8s.io/kustomize/pkg/loader"
+	"sigs.k8s.io/kustomize/pkg/resmap"
+	"sigs.k8s.io/kustomize/pkg/resource"
+	"sigs.k8s.io/kustomize/pkg/target"
+	"testing"
+)
+
+func writeKatibV1Alpha1UIBase(th *KustTestHarness) {
+	th.writeF("/manifests/katib-v1alpha1/katib-ui/base/katib-ui-deployment.yaml", `
+apiVersion: extensions/v1beta1
+kind: Deployment
+metadata:
+  name: katib-ui
+  labels:
+    component: ui
+spec:
+  replicas: 1
+  template:
+    metadata:
+      name: katib-ui
+      labels:
+        component: ui
+    spec:
+      containers:
+      - name: katib-ui
+        image: gcr.io/kubeflow-images-public/katib/katib-ui:v0.1.2-alpha-156-g4ab3dbd
+        command:
+          - './katib-ui'
+        ports:
+        - name: ui
+          containerPort: 80
+      serviceAccountName: katib-ui
+`)
+	th.writeF("/manifests/katib-v1alpha1/katib-ui/base/katib-ui-rbac.yaml", `
+kind: ClusterRole
+apiVersion: rbac.authorization.k8s.io/v1
+metadata:
+  name: katib-ui
+rules:
+- apiGroups:
+  - ""
+  resources:
+  - configmaps
+  verbs:
+  - "*"
+- apiGroups:
+  - kubeflow.org
+  resources:
+  - studyjobs
+  verbs:
+  - "*"
+---
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: katib-ui
+---
+kind: ClusterRoleBinding
+apiVersion: rbac.authorization.k8s.io/v1
+metadata:
+  name: katib-ui
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: katib-ui
+subjects:
+- kind: ServiceAccount
+  name: katib-ui
+`)
+	th.writeF("/manifests/katib-v1alpha1/katib-ui/base/katib-ui-service.yaml", `
+apiVersion: v1
+kind: Service
+metadata:
+  name: katib-ui
+  labels:
+    component: ui
+spec:
+  type: ClusterIP
+  ports:
+    - port: 80
+      protocol: TCP
+      name: ui
+  selector:
+    component: ui
+`)
+	th.writeF("/manifests/katib-v1alpha1/katib-ui/base/params.env", `
+clusterDomain=cluster.local
+`)
+	th.writeK("/manifests/katib-v1alpha1/katib-ui/base", `
+namespace: kubeflow
+resources:
+- katib-ui-deployment.yaml
+- katib-ui-rbac.yaml
+- katib-ui-service.yaml
+configMapGenerator:
+- name: katib-parameters
+  env: params.env
+generatorOptions:
+  disableNameSuffixHash: true
+images:
+  - name: gcr.io/kubeflow-images-public/katib/katib-ui
+    newTag: v0.1.2-alpha-157-g3d4cd04
+vars:
+- name: clusterDomain
+  objref:
+    kind: ConfigMap
+    name: katib-parameters
+    apiVersion: v1
+  fieldref:
+    fieldpath: data.clusterDomain
+- name: namespace
+  objref:
+    kind: Service
+    name: katib-ui
+    apiVersion: v1
+  fieldref:
+    fieldpath: metadata.namespace
+`)
+}
+
+func TestKatibV1Alpha1UIBase(t *testing.T) {
+	th := NewKustTestHarness(t, "/manifests/katib-v1alpha1/katib-ui/base")
+	writeKatibV1Alpha1UIBase(th)
+	m, err := th.makeKustTarget().MakeCustomizedResMap()
+	if err != nil {
+		t.Fatalf("Err: %v", err)
+	}
+	targetPath := "../katib-v1alpha1/katib-ui/base"
+	fsys := fs.MakeRealFS()
+	_loader, loaderErr := loader.NewLoader(targetPath, fsys)
+	if loaderErr != nil {
+		t.Fatalf("could not load kustomize loader: %v", loaderErr)
+	}
+	rf := resmap.NewFactory(resource.NewFactory(kunstruct.NewKunstructuredFactoryImpl()))
+	kt, err := target.NewKustTarget(_loader, rf, transformer.NewFactoryImpl())
+	if err != nil {
+		th.t.Fatalf("Unexpected construction error %v", err)
+	}
+	n, err := kt.MakeCustomizedResMap()
+	if err != nil {
+		t.Fatalf("Err: %v", err)
+	}
+	expected, err := n.EncodeAsYaml()
+	th.assertActualEqualsExpected(m, string(expected))
+}
