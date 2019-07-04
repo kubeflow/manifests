e448fba3f6672f2f98abafb5d991475531cb8e20
Kunming Qu <37601826+kunmingg@users.noreply.github.com>
2019-07-04 14:00:34 -0700

    update image for v0.6.0 (#192)
    * WIP update image for v0.6.0
    
    * update
    
    * update pytorch images

--- tests/tf-job-operator-overlays-application_test.go
+++ tests/tf-job-operator-overlays-application_test.go
@@ -294,7 +294,7 @@ spec:
           valueFrom:
             fieldRef:
               fieldPath: metadata.namespace
-        image: gcr.io/kubeflow-images-public/tf_operator:v0.5.3
+        image: gcr.io/kubeflow-images-public/tf_operator:v0.6.0.rc0
         name: tf-job-dashboard
         ports:
         - containerPort: 8080
@@ -326,7 +326,7 @@ spec:
           valueFrom:
             fieldRef:
               fieldPath: metadata.name
-        image: gcr.io/kubeflow-images-public/tf_operator:v0.5.3
+        image: gcr.io/kubeflow-images-public/tf_operator:v0.6.0.rc0
         name: tf-job-operator
         volumeMounts:
         - mountPath: /etc/config
