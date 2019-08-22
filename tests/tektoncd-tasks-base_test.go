4ae2187b71e1d1a0b653c6384cb150b47cfde032
Kam D Kasravi <kam.d.kasravi@intel.com>
2019-08-21 18:36:03 -0700

    give tekoncd-pipelines ClusterRoleBinding cluster-admin since it's deploying kf

--- tests/tektoncd-tasks-base_test.go
+++ tests/tektoncd-tasks-base_test.go
@@ -67,7 +67,7 @@ metadata:
 roleRef:
   apiGroup: rbac.authorization.k8s.io
   kind: ClusterRole
-  name: tekton-pipelines-admin
+  name: cluster-admin
 subjects:
 - kind: ServiceAccount
   name: tekton-pipelines
