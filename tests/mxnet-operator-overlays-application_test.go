a698191b107405fc7508479018b51d8ddf004160
Kam D Kasravi <kam.d.kasravi@intel.com>
2019-10-08 16:07:17 -0700

    add application overlay for admission-webhook/{webhook,bootstrap}

--- tests/mxnet-operator-overlays-application_test.go
+++ tests/mxnet-operator-overlays-application_test.go
@@ -82,7 +82,8 @@ roleRef:
   name: mxnet-operator
 subjects:
 - kind: ServiceAccount
-  name: mxnet-operator`)
+  name: mxnet-operator
+`)
 	th.writeF("/manifests/mxnet-job/mxnet-operator/base/cluster-role.yaml", `
 apiVersion: rbac.authorization.k8s.io/v1beta1
 kind: ClusterRole
@@ -132,7 +133,8 @@ rules:
   resources:
   - deployments
   verbs:
-  - '*'`)
+  - '*'
+`)
 	th.writeF("/manifests/mxnet-job/mxnet-operator/base/crd.yaml", `
 apiVersion: apiextensions.k8s.io/v1beta1
 kind: CustomResourceDefinition
@@ -184,7 +186,8 @@ kind: ServiceAccount
 metadata:
   labels:
     app: mxnet-operator
-  name: mxnet-operator`)
+  name: mxnet-operator
+`)
 	th.writeK("/manifests/mxnet-job/mxnet-operator/base", `
 apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
