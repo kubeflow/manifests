b637ddccf5c615e3d7af08daadf06be2911b233c
Kam D Kasravi <kam.d.kasravi@intel.com>
2019-10-14 14:20:08 -0700

    revert hack/gen-test-target.sh, hack/gen-test-targets.sh. Checked in accidentally

--- hack/gen-test-targets.sh
+++ hack/gen-test-targets.sh
@@ -6,109 +6,41 @@
 # The script is based on kusttestharness_test.go from kubernetes-sigs/pkg/kusttest/kusttestharness.go
 #
 add_app=''
-all=false
-changed=false
 dry_run=false
-
-if [[ $(basename $PWD) != "manifests" ]]; then
-  echo "must be at manifests root directory to run $0"
-  exit 1
-fi
-
-if [[ ! -f hack/utils.sh ]]; then
-  echo "$PWD/hack/utils.sh doesn't exist"
-  exit 1
-fi
-
 source hack/utils.sh
 
-usage() 
+usage () 
 {
-  echo -e "Usage: $0 [OPTIONS] [<directory>]\n"\
+  echo -e "Usage: $0 [OPTIONS] <directory>\n"\
   'OPTIONS:\n'\
   '  -h | --help       \n'\
-  '     | --add-app <name=version> <directory>\n'\
-  '  -a | --all\n'\
-  '  -c | --changed-only\n'\
-  '  -d | --dry-run'
-}
-
-usage-extended()
-{
-  echo ''
-  echo 'Examples:'
-  echo '1. Generate all unit tests.'
-  echo '   $ '$0' --all'
-  echo '   generating bootstrap-overlays-application_test.go from manifests/admission-webhook/bootstrap/overlays/application'
-  echo '   generating bootstrap-base_test.go from manifests/admission-webhook/bootstrap/base'
-  echo '   generating webhook-overlays-application_test.go from manifests/admission-webhook/webhook/overlays/application'
-  echo '   generating webhook-base_test.go from manifests/admission-webhook/webhook/base'
-  echo '   ...'
-  echo '2. Generate an application overlay for spartakus.'
-  echo '   $ '$0' --add-app spartakus=v0.7.0 common/spartakus'
-  echo '   mkdir -p common/spartakus/overlays/application'
-  echo '   git add common/spartakus/overlays/application'
-  echo '   generate common/spartakus/overlays/application'
-  echo '3. Generate unit tests for just the changed resources on the current branch.'
-  echo '   $ '$0' --changed-only'
-  echo '   generating webhook-overlays-application_test.go from manifests/admission-webhook/webhook/overlays/application'
-  echo '4. Show what unit tests would be generated for just the changed resources on the current branch.'
-  echo '   $ '$0' --changed-only --dry-run'
-  echo '   generating webhook-overlays-application_test.go from manifests/admission-webhook/webhook/overlays/application'
-}
-
-lsbranchname()
-{
-  git branch|grep '^*'|awk '{print $2}'
-}
-
-findremotebranch()
-{
-  git ls-remote --heads $(git config remote.origin.url) $(lsbranchname) | wc -l
+  '     | --add-app <name=version>\n'\
+  '     | --dry-run\n'
 }
 
 findcommand()
 {
+#  find * -type d -exec sh -c '(ls -p "{}"|grep />/dev/null)||echo "{}"' \; | egrep -v 'doc|tests|hack|plugins'
   _findcommand()
   {
-    local branch i
-    case "$all" in 
-      "false")
-        branch=$(lsbranchname)
-        for i in $(git diff --name-only origin/${branch}..upstream/master | egrep -v 'doc|tests|hack|plugins');do 
-          if [[ -f $i ]]; then   
-            echo $(dirname $i); 
-          fi
-        done
-        ;;
-      "true")
-        for i in $(find * -type d -exec sh -c '(ls -p "{}"|grep />/dev/null)||echo "{}"' \; | egrep -v 'doc|tests|hack|plugins|overlays'); do
-          if [[ -d $i ]]; then   
-            echo $(dirname $i); 
-          fi
-        done
-        ;;
-      *)
-        echo "Unknown arguments: $@"
-        exit 1
-        ;;
-    esac
+    local branch=$1
+    for i in $(git diff --name-only origin/${branch}..upstream/master | egrep -v 'doc|tests|hack|plugins');do 
+      if [[ -f $i ]]; then   
+        echo $(dirname $i); 
+      fi
+    done
   }
-  ! $all && (( $(findremotebranch) == 0 )) && \
-  echo "Branch: origin/$(lsbranchname) doesn't exist, push local changes first" >&2 && exit 1
-  _findcommand | sort | uniq
+  _findcommand $(git branch|grep '^*'|awk '{print $2}') | sort | uniq
 }
 
 addapp()
 {
-  local app=${1%=*} version=${1#*=} appdir=${2}/overlays/application cmd
+  local app=${1%=*} version=${1#*=} appdir=${2}/overlays/application 
 #echo 'appdir='$appdir' app='$app' version='$version
   if [[ ! -d $appdir ]]; then
-    cmd="mkdir -p $appdir"
-    $dry_run && cmd='echo '$cmd
-    eval $cmd
+    mkdir -p $appdir
   fi
-  $dry_run || cat << KUSTOMIZATION > ${appdir}/kustomization.yaml
+  cat << KUSTOMIZATION > ${appdir}/kustomization.yaml
 apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
 bases:
@@ -124,7 +56,7 @@ commonLabels:
   app.kubernetes.io/version: $version
 KUSTOMIZATION
 
-  $dry_run || cat << APPLICATION > ${appdir}/application.yaml
+  cat << APPLICATION > ${appdir}/application.yaml
 apiVersion: app.k8s.io/v1beta1
 kind: Application
 metadata:
@@ -157,10 +89,7 @@ spec:
       url: ""
   addOwnerRef: true
 APPLICATION
-
-  cmd="git add $appdir"
-  $dry_run && cmd='echo '$cmd
-  eval $cmd
+  git add $appdir
 }
 
 generate()
@@ -185,7 +114,11 @@ generate()
 
 generate-all()
 {
-  EXCLUDE_DIRS=( "kfdef" "gatekeeper" "gcp/deployment_manager_configs" "aws/infra_configs" "." )
+  if [[ $(basename $PWD) != "manifests" ]]; then
+    echo "must be at manifests root directory to run $0"
+    exit 1
+  fi
+  EXCLUDE_DIRS=( "kfdef" "gatekeeper" "gcp/deployment_manager_configs" "aws/infra_configs" )
   source hack/utils.sh
   for i in $(findcommand); do
     exclude=false
@@ -207,12 +140,8 @@ generate-all()
 while :
 do
   case "$1" in
-    -h)
+    -h | --help)
       usage
-      exit 0
-      ;;
-    --help)
-      usage && usage-extended
       exit 0
       ;;
     --add-app)
@@ -220,15 +149,7 @@ do
       add_app=$1
       shift
       ;;
-    -a | --all)
-      all=true
-      shift
-      ;;
-    -c | --changed-only)
-      changed=true
-      shift
-      ;;
-    -d | --dry-run)
+    --dry-run)
       dry_run=true
       shift
       ;;
@@ -244,22 +165,19 @@ done
 
 case $# in 
   0)
-     ( $all || $changed ) && generate-all || \
-     ( echo -e 'Either --all or --changed-only or directory required\n' && usage && exit 1 )
+     generate-all
      ;;
   1)
      if (( $# == 1 )); then
-       cmd="generate $1"
        if [[ -n $add_app ]]; then
          addapp $add_app $1
-         cmd="generate $1/overlays/application"
        fi
-       $dry_run && cmd='echo '$cmd
-       eval $cmd
+       generate $1
      fi
      ;;
   *)
-     echo "Unknown arguments: $@"
+     echo "unknown arguments $@"
+     usage
      exit 1
      ;;
 esac
