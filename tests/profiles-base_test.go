c0d76bd7cb803d44bd5ac30c907b622796fbc25e
kunming <kunming@google.com>
2019-10-14 10:53:54 -0700

    tests auto update

--- tests/profiles-base_test.go
+++ tests/profiles-base_test.go
@@ -14,22 +14,37 @@ import (
 )
 
 func writeProfilesBase(th *KustTestHarness) {
+	th.writeF("/manifests/profiles/base/cluster-role-binding.yaml", `
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: profiles-cluster-role-binding
+  namespace: kubeflow
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: cluster-admin
+subjects:
+- kind: ServiceAccount
+  name: profiles-controller-service-account
+`)
 	th.writeF("/manifests/profiles/base/crd.yaml", `
 apiVersion: apiextensions.k8s.io/v1beta1
 kind: CustomResourceDefinition
 metadata:
   creationTimestamp: null
-  labels:
-    controller-tools.k8s.io: "1.0"
-  name: profiles.kubeflow.org
+  name: profiles.profile.kubeflow.org
 spec:
-  group: kubeflow.org
+  group: profile.kubeflow.org
   names:
     kind: Profile
     plural: profiles
   scope: Cluster
+  subresources:
+    status: {}
   validation:
     openAPIV3Schema:
+      description: Profile is the Schema for the profiles API
       properties:
         apiVersion:
           description: 'APIVersion defines the versioned schema of this representation
@@ -44,22 +59,127 @@ spec:
         metadata:
           type: object
         spec:
+          description: ProfileSpec defines the desired state of Profile
           properties:
             owner:
               description: The profile owner
+              properties:
+                apiGroup:
+                  description: APIGroup holds the API group of the referenced subject.
+                    Defaults to "" for ServiceAccount subjects. Defaults to "rbac.authorization.k8s.io"
+                    for User and Group subjects.
+                  type: string
+                kind:
+                  description: Kind of object being referenced. Values defined by
+                    this API group are "User", "Group", and "ServiceAccount". If the
+                    Authorizer does not recognized the kind value, the Authorizer
+                    should report an error.
+                  type: string
+                name:
+                  description: Name of the object being referenced.
+                  type: string
+              required:
+                - kind
+                - name
+              type: object
+            plugins:
+              items:
+                description: Plugin is for customize actions on different platform.
+                properties:
+                  apiVersion:
+                    description: 'APIVersion defines the versioned schema of this
+                      representation of an object. Servers should convert recognized
+                      schemas to the latest internal value, and may reject unrecognized
+                      values. More info: https://git.k8s.io/community/contributors/devel/api-conventions.md#resources'
+                    type: string
+                  kind:
+                    description: 'Kind is a string value representing the REST resource
+                      this object represents. Servers may infer this from the endpoint
+                      the client submits requests to. Cannot be updated. In CamelCase.
+                      More info: https://git.k8s.io/community/contributors/devel/api-conventions.md#types-kinds'
+                    type: string
+                  spec:
+                    type: object
+                type: object
+              type: array
+            resourcequotaspec:
+              description: Resourcequota that will be applied to target namespace
+              properties:
+                hard:
+                  additionalProperties:
+                    type: string
+                  description: 'hard is the set of desired hard limits for each named
+                    resource. More info: https://kubernetes.io/docs/concepts/policy/resource-quotas/'
+                  type: object
+                scopeSelector:
+                  description: scopeSelector is also a collection of filters like
+                    scopes that must match each object tracked by a quota but expressed
+                    using ScopeSelectorOperator in combination with possible values.
+                    For a resource to match, both scopes AND scopeSelector (if specified
+                    in spec), must be matched.
+                  properties:
+                    matchExpressions:
+                      description: A list of scope selector requirements by scope
+                        of the resources.
+                      items:
+                        description: A scoped-resource selector requirement is a selector
+                          that contains values, a scope name, and an operator that
+                          relates the scope name and values.
+                        properties:
+                          operator:
+                            description: Represents a scope's relationship to a set
+                              of values. Valid operators are In, NotIn, Exists, DoesNotExist.
+                            type: string
+                          scopeName:
+                            description: The name of the scope that the selector applies
+                              to.
+                            type: string
+                          values:
+                            description: An array of string values. If the operator
+                              is In or NotIn, the values array must be non-empty.
+                              If the operator is Exists or DoesNotExist, the values
+                              array must be empty. This array is replaced during a
+                              strategic merge patch.
+                            items:
+                              type: string
+                            type: array
+                        required:
+                          - operator
+                          - scopeName
+                        type: object
+                      type: array
+                  type: object
+                scopes:
+                  description: A collection of filters that must match each object
+                    tracked by a quota. If not specified, the quota matches all objects.
+                  items:
+                    description: A ResourceQuotaScope defines a filter that must match
+                      each object tracked by a quota
+                    type: string
+                  type: array
               type: object
           type: object
         status:
+          description: ProfileStatus defines the observed state of Profile
           properties:
-            message:
-              type: string
-            status:
-              description: 'INSERT ADDITIONAL STATUS FIELD - define observed state
-                of cluster Important: Run "make" to regenerate code after modifying
-                this file'
-              type: string
+            conditions:
+              items:
+                properties:
+                  message:
+                    type: string
+                  status:
+                    type: string
+                  type:
+                    type: string
+                type: object
+              type: array
           type: object
-  version: v1alpha1
+      type: object
+  version: v1beta1
+  versions:
+    - name: v1beta1
+      served: true
+      storage: true
 status:
   acceptedNames:
     kind: ""
@@ -67,74 +187,14 @@ status:
   conditions: []
   storedVersions: []
 `)
-	th.writeF("/manifests/profiles/base/service-account.yaml", `
----
-apiVersion: v1
-kind: ServiceAccount
-metadata:
-  name: controller-service-account
----
-apiVersion: v1
-kind: ServiceAccount
-metadata:
-  name: default-service-account
-`)
-	th.writeF("/manifests/profiles/base/cluster-role-binding.yaml", `
-apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRoleBinding
-metadata:
-  name: cluster-role-binding
-roleRef:
-  apiGroup: rbac.authorization.k8s.io
-  kind: ClusterRole
-  name: cluster-admin
-subjects:
-- kind: ServiceAccount
-  name: controller-service-account
-`)
-	th.writeF("/manifests/profiles/base/role.yaml", `
-apiVersion: rbac.authorization.k8s.io/v1
-kind: Role
-metadata:
-  name: default-role
-rules:
-- apiGroups:
-  - kubeflow.org
-  resources:
-  - profiles
-  verbs:
-  - create
-  - watch
-  - list
-`)
-	th.writeF("/manifests/profiles/base/role-binding.yaml", `
-apiVersion: rbac.authorization.k8s.io/v1
-kind: RoleBinding
-metadata:
-  name: default-role-binding
-roleRef:
-  apiGroup: rbac.authorization.k8s.io
-  kind: Role
-  name: default-role
-subjects:
-- kind: ServiceAccount
-  name: default-service-account
-`)
-	th.writeF("/manifests/profiles/base/service.yaml", `
-apiVersion: v1
-kind: Service
-metadata:
-  name: kfam
-spec:
-  ports:
-    - port: 8081
-`)
 	th.writeF("/manifests/profiles/base/deployment.yaml", `
 apiVersion: apps/v1
 kind: Deployment
 metadata:
-  name: deployment
+  name: profiles-deployment
+  namespace: kubeflow
 spec:
+  replicas: 1
   template:
     spec:
       containers:
@@ -145,6 +205,8 @@ spec:
         - $(userid-header)
         - "-userid-prefix"
         - $(userid-prefix)
+        - "-workload-identity"
+        - $(gcp-sa)
         image: gcr.io/kubeflow-images-public/profile-controller:v20190619-v0-219-gbd3daa8c-dirty-1ced0e
         imagePullPolicy: Always
         name: manager
@@ -162,11 +224,40 @@ spec:
         name: kfam
       serviceAccountName: controller-service-account
 `)
+	th.writeF("/manifests/profiles/base/profile-instance.yaml", `
+apiVersion: profile.kubeflow.org/v1beta1
+kind: Profile
+metadata:
+  name: $(profile-name)
+spec:
+  owner:
+    kind: User
+    name: $(admin)
+`)
+	th.writeF("/manifests/profiles/base/service.yaml", `
+apiVersion: v1
+kind: Service
+metadata:
+  name: profiles-kfam
+  namespace: kubeflow
+spec:
+  ports:
+    - port: 8081
+`)
+	th.writeF("/manifests/profiles/base/service-account.yaml", `
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: profiles-controller-service-account
+  namespace: kubeflow
+`)
 	th.writeF("/manifests/profiles/base/params.yaml", `
 varReference:
 - path: spec/template/spec/containers/0/args/1
   kind: Deployment
 - path: spec/template/spec/containers/0/args/3
+  kind: Deployment
+- path: spec/template/spec/containers/0/args/5
   kind: Deployment
 - path: spec/template/spec/containers/1/args/1
   kind: Deployment
@@ -174,9 +265,15 @@ varReference:
   kind: Deployment
 - path: spec/template/spec/containers/1/args/5
   kind: Deployment
+- path: spec/owner/name
+  kind: Profile
+- path: metadata/name
+  kind: Profile
 `)
 	th.writeF("/manifests/profiles/base/params.env", `
-admin=
+admin=anonymous
+profile-name=anonymous
+gcp-sa=
 userid-header=
 userid-prefix=
 `)
@@ -184,15 +281,12 @@ userid-prefix=
 apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
 resources:
-- crd.yaml
-- service-account.yaml
 - cluster-role-binding.yaml
-- role.yaml
-- role-binding.yaml
-- service.yaml
+- crd.yaml
 - deployment.yaml
-namePrefix: profiles-
-namespace: kubeflow
+- profile-instance.yaml
+- service.yaml
+- service-account.yaml
 commonLabels:
   kustomize.component: profiles
 configMapGenerator:
@@ -200,11 +294,9 @@ configMapGenerator:
   env: params.env
 images:
 - name: gcr.io/kubeflow-images-public/profile-controller
-  newName: gcr.io/kubeflow-images-public/profile-controller
-  newTag: v20190619-v0-219-gbd3daa8c-dirty-1ced0e
+  digest: sha256:00873cfa9bba52565cccfe57f6f76a5807657536488114236f1da800d56128eb
 - name: gcr.io/kubeflow-images-public/kfam
-  newName: gcr.io/kubeflow-images-public/kfam
-  newTag: v20190612-v0-170-ga06cdb79-dirty-a33ee4
+  digest: sha256:59564e438d1ba9d956d01d8e5cc52d8bb5de6c6cce065ccc3dac3b396fc6f5cc
 vars:
 - name: admin
   objref:
@@ -213,6 +305,20 @@ vars:
     apiVersion: v1
   fieldref:
     fieldpath: data.admin
+- name: gcp-sa
+  objref:
+    kind: ConfigMap
+    name: profiles-parameters
+    apiVersion: v1
+  fieldref:
+    fieldpath: data.gcp-sa
+- name: profile-name
+  objref:
+    kind: ConfigMap
+    name: profiles-parameters
+    apiVersion: v1
+  fieldref:
+    fieldpath: data.profile-name
 - name: userid-header
   objref:
     kind: ConfigMap
@@ -230,7 +336,7 @@ vars:
 - name: namespace
   objref:
     kind: Service
-    name: kfam
+    name: profiles-kfam
     apiVersion: v1
   fieldref:
     fieldpath: metadata.namespace
