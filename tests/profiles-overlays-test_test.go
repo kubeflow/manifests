acc18c8cdb544c6999f611563cccb7f095c041fb
Yannis Zarkadas <yanniszark@arrikto.com>
2019-10-18 15:54:16 +0300

    regenerate tests
    Signed-off-by: Yannis Zarkadas <yanniszark@arrikto.com>

--- tests/profiles-overlays-test_test.go
+++ tests/profiles-overlays-test_test.go
@@ -192,8 +192,7 @@ status:
     kind: ""
     plural: ""
   conditions: []
-  storedVersions: []
-`)
+  storedVersions: []`)
 	th.writeF("/manifests/profiles/base/deployment.yaml", `
 apiVersion: apps/v1
 kind: Deployment
@@ -237,8 +236,7 @@ metadata:
   name: kfam
 spec:
   ports:
-    - port: 8081
-`)
+    - port: 8081`)
 	th.writeF("/manifests/profiles/base/service-account.yaml", `
 apiVersion: v1
 kind: ServiceAccount
