a06a2ddee5e52bc524bf1461f0ddbaee243b4e74
Richard Liu <ricliu@google.com>
2020-01-28 13:56:57 -0800

    fix component name

--- tests/katib-katib-controller-overlays-istio_test.go
+++ tests/katib-katib-controller-overlays-istio_test.go
@@ -310,19 +310,19 @@ metadata:
   name: katib-mysql
   labels:
     app: katib
-    component: db
+    component: mysql
 spec:
   replicas: 1
   selector:
     matchLabels:
       app: katib
-      component: db
+      component: mysql
   template:
     metadata:
       name: katib-mysql
       labels:
         app: katib
-        component: db
+        component: mysql
     spec:
       containers:
       - name: katib-mysql
@@ -397,7 +397,7 @@ metadata:
   name: katib-mysql
   labels:
     app: katib
-    component: db
+    component: mysql
 spec:
   type: ClusterIP
   ports:
@@ -406,7 +406,7 @@ spec:
       name: dbapi
   selector:
     app: katib
-    component: db
+    component: mysql
 `)
 	th.writeF("/manifests/katib/katib-controller/base/katib-db-manager-deployment.yaml", `
 apiVersion: apps/v1
