422effbd5c7286685ec751a7c611ea993a7093ed
krishnadurai <krishnadurai20@gmail.com>
2020-02-25 14:52:57 +0530

    Adds kfserving ingressgateway for Istio 1.1.6 Modifies KFServing and KNative Serving config-maps to use this gateway

--- tests/kfserving-kfserving-install-overlays-application_test.go
+++ tests/kfserving-kfserving-install-overlays-application_test.go
@@ -401,8 +401,8 @@ data:
     }
   ingress: |-
     {
-        "ingressGateway" : "kubeflow-gateway.kubeflow",
-        "ingressService" : "istio-ingressgateway.istio-system.svc.cluster.local"
+        "ingressGateway" : "knative-serving.knative-ingress-gateway",
+        "ingressService" : "kfserving-ingressgateway.istio-system.svc.cluster.local"
     }
   logger: |-
     {
--- tests/knative-knative-serving-install-base_test.go
+++ tests/knative-knative-serving-install-base_test.go
@@ -15,7 +15,6 @@ import (
 
 func writeKnativeServingInstallBase(th *KustTestHarness) {
 	th.writeF("/manifests/knative/knative-serving-install/base/gateway.yaml", `
----
 apiVersion: networking.istio.io/v1alpha3
 kind: Gateway
 metadata:
@@ -34,6 +33,26 @@ spec:
         name: http
         number: 80
         protocol: HTTP
+
+---
+apiVersion: networking.istio.io/v1alpha3
+kind: Gateway
+metadata:
+  labels:
+    networking.knative.dev/ingress-provider: istio
+  name: knative-ingress-gateway
+  namespace: knative-serving
+spec:
+  selector:
+     app: kfserving-ingressgateway
+     kfserving: ingressgateway
+  servers:
+  - hosts:
+    - '*'
+    port:
+      name: http
+      number: 80
+      protocol: HTTP
 `)
 	th.writeF("/manifests/knative/knative-serving-install/base/cluster-role.yaml", `
 ---
@@ -1062,7 +1081,7 @@ data:
     # {{ingress_namespace}}.svc.cluster.local"`+"`"+`. The {{gateway_namespace}}
     # is optional; when it is omitted, the system will search for
     # the gateway in the serving system namespace `+"`"+`knative-serving`+"`"+`
-    gateway.kubeflow.kubeflow-gateway: "istio-ingressgateway.istio-system.svc.cluster.local"
+    gateway.knative-serving.knative-ingress-gateway: "kfserving-ingressgateway.istio-system.svc.cluster.local"
 
     # A cluster local gateway to allow pods outside of the mesh to access
     # Services and Routes not exposing through an ingress.  If the users
