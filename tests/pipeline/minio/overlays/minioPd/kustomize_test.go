c2e48b9e546aeaf5e4933f4ddbe38dd67884f944
Jeremy Lewi <jlewi@google.com>
2020-02-27 16:45:34 -0800

    Prototype of how we can replace kfctl magic with built in kustomization features
    * For centraldashboard define a kustomization file so that we don't need
      to rely on generation by kfctl
    
    * For centraldashboard define a patch that sets environment variables
      based on a "global" kubeflow config map. This way we can have
      single configmap that is reused across applications for parameters
      that should be shared across applications
    
    * Define a "stack" a stack is an oppionated way of combining applications
      * a stack is oppinionated about which applications to include and how
        to configure them
      * So we would define a GCP stack that would contain the apps to be
        included in GCP deployments.
    
      * Stacks can be used to replace much of the functionality currently
        achieved by the list of applications in KFDef
    
        * Instead of listing applications in KFDef we could just point to a
          kustomization file containing the list of applications.
    
    * Define an example illustrating how alice would define an overlay
      to add her kustomizations.
    
      * Show how she can define a patch to modify the additional configuration
        parameters for her deployment.
    
    Modify centraldashboard so that we can work with stacks without impacting
    current workflows.
    
    * Create common/centraldashboard/overlays/stacks to define the new package
      which is uses the global config.
    
    * Change stacks/gcp/kustomization.yaml to use it.
    
    Refactor changes to centraldashboard so that we don't impact existing
    configs.
    
    Get rid of core. We don't need it. We can just do kustomize build --load_restrictor none.

---
+++ tests/pipeline/minio/overlays/minioPd/kustomize_test.go
@@ -0,0 +1,15 @@
+package minioPd
+
+import (
+	"github.com/kubeflow/manifests/tests"
+	"testing"
+)
+
+func TestKustomize(t *testing.T) {
+	testCase := &tests.KustomizeTestCase{
+		Package:  "../../../../../pipeline/minio/overlays/minioPd",
+		Expected: "test_data/expected",
+	}
+
+	tests.RunTestCase(t, testCase)
+}
