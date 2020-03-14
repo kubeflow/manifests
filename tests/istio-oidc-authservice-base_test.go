19148c529391fd69ac01122e56cd04bf721945e9
Kunming Qu <37601826+kunmingg@users.noreply.github.com>
2020-03-13 17:04:34 -0700

    Experimental GCP config for kubeflow setup (#971)
    * kubeflow asm config
    
    * exp config for GCP setup
    
    * update tests
    
    * exp kfdef should use master
    
    * resolve comments
    
    * remove exp DM entry
    
    * add reeadme; rename folder

--- tests/istio-oidc-authservice-base_test.go
+++ tests/istio-oidc-authservice-base_test.go
@@ -27,7 +27,8 @@ spec:
   - port: 8080
     name: http-authservice
     targetPort: http-api
-  publishNotReadyAddresses: true`)
+  publishNotReadyAddresses: true
+`)
 	th.writeF("/manifests/istio/oidc-authservice/base/statefulset.yaml", `
 apiVersion: apps/v1
 kind: StatefulSet
@@ -136,7 +137,8 @@ spec:
     - ReadWriteOnce
   resources:
     requests:
-      storage: 10Gi`)
+      storage: 10Gi
+`)
 	th.writeF("/manifests/istio/oidc-authservice/base/params.yaml", `
 varReference:
 - path: spec/template/spec/containers/env/value
@@ -144,7 +146,8 @@ varReference:
 - path: spec/filters/filterConfig/httpService/serverUri/uri
   kind: EnvoyFilter
 - path: spec/filters/filterConfig/httpService/serverUri/cluster
-  kind: EnvoyFilter`)
+  kind: EnvoyFilter
+`)
 	th.writeF("/manifests/istio/oidc-authservice/base/params.env", `
 client_id=ldapdexapp
 oidc_provider=
@@ -154,7 +157,8 @@ application_secret=pUBnBOY80SnXgjibTYM9ZWNzY2xreNGQok
 skip_auth_uri=
 userid-header=
 userid-prefix=
-namespace=istio-system`)
+namespace=istio-system
+`)
 	th.writeK("/manifests/istio/oidc-authservice/base", `
 apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
