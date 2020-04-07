b3fdd12f4a41da11ca18430506ff703166918562
Richard Liu <39319471+richardsliu@users.noreply.github.com>
2020-04-07 14:25:44 -0700

    Cherry pick 949 - Kfserving gateway (#1072)
    * Adds kfserving ingressgateway for Istio 1.1.6 (#949)
    
    * Adds kfserving ingressgateway for Istio 1.1.6
    Modifies KFServing and KNative Serving config-maps to use this gateway
    
    * Updates configs for Istio 1.1.6
    
    * Updates tests for istio 1.1.6
    
    * Adding kfserving gateway to kfdefs having istio-1-1-6
    
    * Gateway name correction for kfserving config
    
    * Adds HPA config to gateway
    
    * revert changes to kfdef
    
    Co-authored-by: Krishna Durai <krishnadurai20@gmail.com>

--- tests/kfserving-kfserving-install-overlays-application_test.go
+++ tests/kfserving-kfserving-install-overlays-application_test.go
@@ -402,8 +402,8 @@ data:
     }
   ingress: |-
     {
-        "ingressGateway" : "kubeflow-gateway.kubeflow",
-        "ingressService" : "istio-ingressgateway.istio-system.svc.cluster.local"
+        "ingressGateway" : "knative-ingress-gateway.knative-serving",
+        "ingressService" : "kfserving-ingressgateway.istio-system.svc.cluster.local"
     }
   logger: |-
     {
