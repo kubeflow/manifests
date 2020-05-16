6d7d6a2dee3cb025059a0ecadc80fe6bc9f8f0a9
Jeremy Lewi <jlewi@google.com>
2020-05-15 18:01:20 -0700

    CRDs should not have status field.
    * kubeflow/manifests#1174 lots of CRDs have status fields which
      causes ACM to complain that the CRD is invalid.
    
    * This PR cleans up those CRDs and adds an appropriate validation test.
    
    * Fix typos in AWS application specs (kubeflow/testing#668)

--- tests/validate_resources_test.go
+++ tests/validate_resources_test.go
@@ -3,8 +3,10 @@ package tests
 import (
 	"github.com/ghodss/yaml"
 	"io/ioutil"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"os"
 	"path/filepath"
+	"regexp"
 	"sigs.k8s.io/kustomize/v3/pkg/types"
 	"testing"
 )
@@ -82,6 +84,73 @@ func TestCommonLabelsImmutable(t *testing.T) {
 			if _, ok := k.CommonLabels[l]; ok {
 				t.Errorf("%v has forbidden commonLabel %v", path, l)
 			}
+		}
+		return nil
+	})
+
+	if err != nil {
+		t.Errorf("error walking the path %v; error: %v", rootDir, err)
+
+	}
+}
+
+// TestNoStatus is a test to try to ensure we don't include status in resources
+// as this causes validation issues: https://github.com/kubeflow/manifests/issues/1174
+func TestNoStatus(t *testing.T) {
+	rootDir := ".."
+
+	// Directories to exclude. Thee paths should be relative to rootDir.
+	// Subdirectories won't be searched
+	excludes := map[string]bool{
+		"tests":   true,
+		".git":    true,
+		".github": true,
+		"profiles/overlays/test": true,
+		// Skip cnrm-install. We don't install this with ACM so we don't need to fix it.
+		// It seems like if this is an issue it should eventually get fixed in upstream cnrm configs.
+		"gcp/v2/management/cnrm-install": true,
+	}
+
+	err := filepath.Walk("..", func(path string, info os.FileInfo, err error) error {
+		relPath, err := filepath.Rel(rootDir, path)
+
+		if err != nil {
+			t.Fatalf("Could not compute relative path(%v, %v); error: %v", rootDir, path, err)
+		}
+
+		if _, ok := excludes[relPath]; ok {
+			return filepath.SkipDir
+		}
+
+		// skip directories
+		if info.IsDir() {
+			return nil
+		}
+
+		// Skip non YAML files
+		ext := filepath.Ext(info.Name())
+
+		if ext != ".yaml" && ext != ".yml" {
+			return nil
+		}
+		data, err := ioutil.ReadFile(path)
+
+		if err != nil {
+			t.Errorf("Error reading %v; error: %v", path, err)
+		}
+
+		u := &unstructured.Unstructured{}
+
+		if err = yaml.Unmarshal(data, u); err != nil {
+			if match, _ := regexp.MatchString(".*Object 'Kind' is missing.*", err.Error()); match {
+				t.Logf("Skipping %v; not a K8s resource;", path)
+				return nil
+			}
+			t.Errorf("Error unmarshaling %v; error: %v", path, err)
+		}
+
+		if _, ok := u.UnstructuredContent()["status"]; ok {
+			t.Errorf("Path %v; has status field", path)
 		}
 		return nil
 	})
