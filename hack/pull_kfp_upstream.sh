9e9c72505233ff516657d7a52957741fa58c7c27
Yuan Gong <gongyuan94@gmail.com>
2020-06-11 14:20:50 +0800

    Pull pipelines manifest from upstream

---
+++ hack/pull_kfp_upstream.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+export PIPELINES_SRC_REPO=https://github.com/kubeflow/pipelines.git
+export PIPELINES_VERSION=1.0.0-rc.1
+# Pulling for the first time
+# kpt pkg get $PIPELINES_SRC_REPO/manifests/kustomize@$PIPELINES_VERSION pipeline/upstream
+
+# Updates
+kpt pkg update pipeline/upstream/@$PIPELINES_VERSION --strategy force-delete-replace
