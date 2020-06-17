88cc01b22965de1102f30ed216d2a79f6861f68a
Yuan Gong <gongyuan94@gmail.com>
2020-06-17 13:32:03 +0800

    Updated kfp to rc2

--- hack/pull_kfp_upstream.sh
+++ hack/pull_kfp_upstream.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export PIPELINES_SRC_REPO=https://github.com/kubeflow/pipelines.git
-export PIPELINES_VERSION=1.0.0-rc.1
+export PIPELINES_VERSION=1.0.0-rc.2
 # Pulling for the first time
 # kpt pkg get $PIPELINES_SRC_REPO/manifests/kustomize@$PIPELINES_VERSION pipeline/upstream
 
