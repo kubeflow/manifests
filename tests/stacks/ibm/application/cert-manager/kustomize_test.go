bc5c7a72cf00960e0678e8bb4f97182e22764018
Adrian Zhuang <wzhuang@us.ibm.com>
2020-06-22 11:12:36 -0700

    update kfctl_ibm KfDef to kustomize v3  (#1246)
    * update kfctl_ibm kfdef to kustomize v3
    
    * small update to README
    
    * update to use katib, minio and mysql generic
    
    * update after platform test
    
    * fix test failure

---
+++ tests/stacks/ibm/application/cert-manager/kustomize_test.go
@@ -0,0 +1,15 @@
+package cert_manager
+
+import (
+	"github.com/kubeflow/manifests/tests"
+	"testing"
+)
+
+func TestKustomize(t *testing.T) {
+	testCase := &tests.KustomizeTestCase{
+		Package:  "../../../../../stacks/ibm/application/cert-manager",
+		Expected: "test_data/expected",
+	}
+
+	tests.RunTestCase(t, testCase)
+}
