2edd7835a04e60edacb1977591f1b73ccf056453
Krishna Durai <krishnadurai20@gmail.com>
2020-08-17 23:33:30 +0530

    v1.1 istio dex components for kubernetes installation (#1494)
    * v1.1 istio dex components for kubernetes installation
    
    * Adds kfdef and creds correction
    
    * Test cases generated
    
    * v1.1.0 kfdef added

---
+++ tests/stacks/kubernetes/kustomize_test.go
@@ -0,0 +1,15 @@
+package kubernetes
+
+import (
+	"github.com/kubeflow/manifests/tests"
+	"testing"
+)
+
+func TestKustomize(t *testing.T) {
+	testCase := &tests.KustomizeTestCase{
+		Package:  "../../../stacks/kubernetes",
+		Expected: "test_data/expected",
+	}
+
+	tests.RunTestCase(t, testCase)
+}
