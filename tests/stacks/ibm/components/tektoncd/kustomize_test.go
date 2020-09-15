220e6a558bf22e379f928162a56d3538e7431dfc
adrian555 <wzhuang@us.ibm.com>
2020-09-15 16:15:37 -0700

    add custom stacks for IBM

---
+++ tests/stacks/ibm/components/tektoncd/kustomize_test.go
@@ -0,0 +1,15 @@
+package tektoncd
+
+import (
+	"github.com/kubeflow/manifests/tests"
+	"testing"
+)
+
+func TestKustomize(t *testing.T) {
+	testCase := &tests.KustomizeTestCase{
+		Package:  "../../../../../stacks/ibm/components/tektoncd",
+		Expected: "test_data/expected",
+	}
+
+	tests.RunTestCase(t, testCase)
+}
