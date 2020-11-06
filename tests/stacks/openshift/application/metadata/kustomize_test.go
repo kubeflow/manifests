c5775f3a8c72c2de7305eea41496316770bc5198
Mofizur Rahman <moficodes@gmail.com>
2020-11-06 16:04:19 -0500

    rebase master

---
+++ tests/stacks/openshift/application/metadata/kustomize_test.go
@@ -0,0 +1,15 @@
+package metadata
+
+import (
+	"github.com/kubeflow/manifests/tests"
+	"testing"
+)
+
+func TestKustomize(t *testing.T) {
+	testCase := &tests.KustomizeTestCase{
+		Package:  "../../../../../stacks/openshift/application/metadata",
+		Expected: "test_data/expected",
+	}
+
+	tests.RunTestCase(t, testCase)
+}
