2a267b0b07c74f3dfc30fdc28174ceb79044a615
Kimonas Sotirchos <kimwnasptd@arrikto.com>
2022-02-10 17:08:00 +0200

    hack: Update pipelines sync script to change README
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@arrikto.com>

--- hack/sync-pipelines-manifests.sh
+++ hack/sync-pipelines-manifests.sh
@@ -55,8 +55,15 @@ cp $SRC_DIR/manifests/kustomize $DST_DIR -r
 
 echo "Successfully copied all manifests."
 
+echo "Updating README..."
+SRC_TXT="\[.*\](https://github.com/kubeflow/pipelines/tree/.*/manifests/kustomize)"
+DST_TXT="\[$COMMIT\](https://github.com/kubeflow/pipelines/tree/$COMMIT/manifests/kustomize)"
+
+sed -i "s|$SRC_TXT|$DST_TXT|g" ${MANIFESTS_DIR}/README.md
+
 # DEV: Comment out these commands when local testing
 echo "Committing the changes..."
 cd $MANIFESTS_DIR
 git add apps
+git add README.md
 git commit -m "Update kubeflow/pipelines manifests from ${COMMIT}"
