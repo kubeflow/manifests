476b48552c45e833eb5bf83ab12fd925c17d605f
Suraj Kota <surakota@amazon.com>
2022-07-01 18:26:14 +0000

    move crd wait outside set +e

--- tests/gh-actions/install_kserve.sh
+++ tests/gh-actions/install_kserve.sh
@@ -5,8 +5,9 @@ cd contrib/kserve
 kubectl create ns kubeflow
 set +e
 kustomize build kserve | kubectl apply -f -
-kubectl wait --for condition=established --timeout=30s crd/clusterservingruntimes.serving.kserve.io
 set -e
+echo "Waiting for crd/clusterservingruntimes.serving.kserve.io to be available ..."
+kubectl wait --for condition=established --timeout=30s crd/clusterservingruntimes.serving.kserve.io
 kustomize build kserve | kubectl apply -f -
 kustomize build models-web-app/overlays/kubeflow | kubectl apply -f -
 kubectl wait --for=condition=Ready pods --all --all-namespaces --timeout 180s
