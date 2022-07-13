ded7493f71ddfad8eec0bad4a270b87bb33879fb
Anna Jung (VMware) <antheaj@vmware.com>
2022-07-13 19:32:06 -0400

    Change pipeline installation command back to apply crd only
    Signed-off-by: Anna Jung (VMware) <antheaj@vmware.com>

--- tests/gh-actions/install_pipelines.sh
+++ tests/gh-actions/install_pipelines.sh
@@ -3,9 +3,7 @@ set -euo pipefail
 echo "Installing Pipelines ..."
 cd apps/pipeline/upstream
 kubectl create ns kubeflow
-set +e
-kustomize build env/cert-manager/platform-agnostic-multi-user | kubectl apply -f -
-set -e
+kubectl apply -f third-party/metacontroller/base/crd.yaml
 echo "Waiting for crd/compositecontrollers.metacontroller.k8s.io to be available ..."
 kubectl wait --for condition=established --timeout=30s crd/compositecontrollers.metacontroller.k8s.io
 kustomize build env/cert-manager/platform-agnostic-multi-user | kubectl apply -f -
