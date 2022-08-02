36c35ba44a3506016e5f457d728de7f43b9c2d00
Nikolaos Loukas <nickloukas@arrikto.com>
2022-08-02 15:34:37 +0300

    Edit install argo cli script.

--- tests/gh-actions/install_argo_cli.sh
+++ tests/gh-actions/install_argo_cli.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
-kubectl create ns argo
+kubectl create namespace argo
 # Download the binary
 curl -sLO https://github.com/argoproj/argo-workflows/releases/download/v3.1.3/argo-linux-amd64.gz
+
 # Unzip
 gunzip argo-linux-amd64.gz
+
 # Make binary executable
 chmod +x argo-linux-amd64
+
 # Move binary to path
-sudo mv ./argo-linux-amd64 /usr/local/bin/argo
+mv ./argo-linux-amd64 /usr/local/bin/argo
