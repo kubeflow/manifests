bb5a15be8c15c9a287b17c9df79732e6af6f3a9a
Anna Jung (VMware) <antheaj@vmware.com>
2022-09-27 09:47:17 -0400

    Increase timeout for pipeline installation
    Signed-off-by: Anna Jung (VMware) <antheaj@vmware.com>

--- tests/gh-actions/install_pipelines.sh
+++ tests/gh-actions/install_pipelines.sh
@@ -7,4 +7,4 @@ kubectl apply -f third-party/metacontroller/base/crd.yaml
 echo "Waiting for crd/compositecontrollers.metacontroller.k8s.io to be available ..."
 kubectl wait --for condition=established --timeout=30s crd/compositecontrollers.metacontroller.k8s.io
 kustomize build env/cert-manager/platform-agnostic-multi-user | kubectl apply -f -
-kubectl wait --for=condition=Ready pods --all --all-namespaces --timeout 180s
+kubectl wait --for=condition=Ready pods --all --all-namespaces --timeout 300s
