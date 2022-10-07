f4810ed177d342afb258b9f983ebb1ffaac4f3cb
Nikolaos Loukas <nickloukas@arrikto.com>
2022-10-07 17:02:24 +0300

    Extend KServe GH action to also apply an inference service and assert it becomes ready (#2266)
    * Extend kserve KinD test workflow to create an Inference Service.
    
    * Fix typo in yaml.

---
+++ tests/gh-actions/install_knative.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -euo pipefail
+echo "Installing KNative ..."
+set +e
+kustomize build common/knative/knative-serving/overlays/gateways | kubectl apply -f -
+set -e
+kustomize build common/knative/knative-serving/overlays/gateways | kubectl apply -f -
+kustomize build common/knative/knative-eventing/base | kubectl apply -f -
+kustomize build common/istio-1-14/kubeflow-istio-resources/base | kubectl apply -f -
