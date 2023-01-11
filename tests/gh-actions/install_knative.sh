8effb5e218d646f8b06f96aaa3282d14981f5961
Andrews Arokiam <andrews.arokiam@ideas2it.com>
2023-01-11 16:24:12 +0530

    Improve ci/cd workflow test
    Signed-off-by: Andrews Arokiam <andrews.arokiam@ideas2it.com>

--- tests/gh-actions/install_knative.sh
+++ tests/gh-actions/install_knative.sh
@@ -5,5 +5,5 @@ set +e
 kustomize build common/knative/knative-serving/overlays/gateways | kubectl apply -f -
 set -e
 kustomize build common/knative/knative-serving/overlays/gateways | kubectl apply -f -
-kustomize build common/knative/knative-eventing/base | kubectl apply -f -
+kustomize build common/istio-1-16/cluster-local-gateway/base | kubectl apply -f -
 kustomize build common/istio-1-16/kubeflow-istio-resources/base | kubectl apply -f -
