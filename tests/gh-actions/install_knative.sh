1002b97054e429f717e749c0e087544a2c7bfea0
Andrews Arokiam <andrews.arokiam@ideas2it.com>
2023-01-26 19:24:50 +0530

    Remove cluster local gateway
    Signed-off-by: Andrews Arokiam <andrews.arokiam@ideas2it.com>

--- tests/gh-actions/install_knative.sh
+++ tests/gh-actions/install_knative.sh
@@ -5,5 +5,4 @@ set +e
 kustomize build common/knative/knative-serving/overlays/gateways | kubectl apply -f -
 set -e
 kustomize build common/knative/knative-serving/overlays/gateways | kubectl apply -f -
-kustomize build common/istio-1-16/cluster-local-gateway/base | kubectl apply -f -
 kustomize build common/istio-1-16/kubeflow-istio-resources/base | kubectl apply -f -
