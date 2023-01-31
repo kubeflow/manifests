adc00b804404ea08685a044ae595be0bed9adb59
Andrews Arokiam <andrews.arokiam@ideas2it.com>
2023-01-31 16:28:03 +0530

    Add istio cluster-local-gateway and remove knative gateway
    Signed-off-by: Andrews Arokiam <andrews.arokiam@ideas2it.com>

--- tests/gh-actions/install_knative.sh
+++ tests/gh-actions/install_knative.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 echo "Installing KNative ..."
-set +e
-kustomize build common/knative/knative-serving/overlays/gateways | kubectl apply -f -
-set -e
-kustomize build common/knative/knative-serving/overlays/gateways | kubectl apply -f -
+
+kustomize build common/knative/knative-serving/base | kubectl apply -f -
+# temporarily repeating to fix issue during installation
+kustomize build common/knative/knative-serving/base | kubectl apply -f -
+
+kustomize build common/istio-1-16/cluster-local-gateway/base | kubectl apply -f -
 kustomize build common/istio-1-16/kubeflow-istio-resources/base | kubectl apply -f -
