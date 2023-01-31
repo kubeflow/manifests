69edcb810b18a33ab868e005d7a52132cc570ade
Andrews Arokiam <andrews.arokiam@ideas2it.com>
2023-02-01 00:14:47 +0530

    Try fixing issue with knative installation
    Most likely a separate PR?
    
    Signed-off-by: Andrews Arokiam <andrews.arokiam@ideas2it.com>

--- tests/gh-actions/install_knative.sh
+++ tests/gh-actions/install_knative.sh
@@ -3,8 +3,6 @@ set -euo pipefail
 echo "Installing KNative ..."
 
 kustomize build common/knative/knative-serving/base | kubectl apply -f -
-# temporarily repeating to fix issue during installation
-kustomize build common/knative/knative-serving/base | kubectl apply -f -
 
 kustomize build common/istio-1-16/cluster-local-gateway/base | kubectl apply -f -
 kustomize build common/istio-1-16/kubeflow-istio-resources/base | kubectl apply -f -
