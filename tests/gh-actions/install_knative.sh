8e5714171f1fd5b00f59f436e9ab8cb45a0f30e3
Kimonas Sotirchos <kimwnasptd@arrikto.com>
2023-02-07 13:46:32 +0200

    Sync MWA manifests from 0.10.0 and show KF version in README (#2381)
    * Update kserve web app manifests from v0.10.0
    
    * Update kserve manifests from v0.10.0
    
    Update kserve/kserve manifests from v0.10.0
    
    * README: Explicitly mention the KF version
    
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@arrikto.com>
    
    * tests: Increase timeouts to 10 mins
    
    We saw that waiting for Pods to become ready could take more than 3
    mins.
    
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@arrikto.com>
    
    ---------
    
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@arrikto.com>

--- tests/gh-actions/install_knative.sh
+++ tests/gh-actions/install_knative.sh
@@ -9,5 +9,5 @@ kustomize build common/knative/knative-serving/base | kubectl apply -f -
 kustomize build common/istio-1-16/cluster-local-gateway/base | kubectl apply -f -
 kustomize build common/istio-1-16/kubeflow-istio-resources/base | kubectl apply -f -
 
-kubectl wait --for=condition=Ready pods --all --all-namespaces --timeout 180s
+kubectl wait --for=condition=Ready pods --all --all-namespaces --timeout 600s
 kubectl patch cm config-domain --patch '{"data":{"example.com":""}}' -n knative-serving
