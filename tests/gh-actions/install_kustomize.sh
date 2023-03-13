cd422e09a5fd1e1e1afd0fea126d0256ec4cb3be
DomFleischmann <dominik.fleischmann@canonical.com>
2023-03-13 17:21:24 +0100

    Update instructions for Kustomize 5 (#2399) (#2408)
    * example: Update for kustomize 5
    
    Add the sortOptions in the kustomization.yaml to ensure we are creating
    MutationWebhooks before Deployments and Pods. This way we'll ensure that
    Pods will get Istio sidecars in a race-free way.
    
    
    
    * readme: Update instructions to use Kustomize 5
    
    Update instructions to inform users to use Kustomize 5. We'll also need
    to do an awk command, to remove a line that ends up in the final output.
    That is a regression on Kustomize's side. Once a newer version of
    Kustomize is available with the fix we'll then remove this awk command.
    
    
    
    * gh-action: Update to Kustomize 5
    
    Update the helper script to use Kustomize 5 for the tests.
    
    
    
    * gh-action: Use the kustomize script in gh action
    
    Update the GH Action that runs the unit test to use the same script we
    use in the other GH Actions for fetching kustomize.
    
    
    
    * Add FAQ for earlier versions of Kustomize
    
    
    
    ---------
    
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@arrikto.com>
    Co-authored-by: Kimonas Sotirchos <kimwnasptd@arrikto.com>

--- tests/gh-actions/install_kustomize.sh
+++ tests/gh-actions/install_kustomize.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
-curl --silent --location --remote-name "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize/v3.2.3/kustomize_kustomize.v3.2.3_linux_amd64"
-chmod a+x kustomize_kustomize.v3.2.3_linux_amd64
-sudo mv kustomize_kustomize.v3.2.3_linux_amd64 /usr/local/bin/kustomize
+curl --silent --location --remote-name "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv5.0.0/kustomize_v5.0.0_linux_amd64.tar.gz"
+tar -xzvf kustomize_v5.0.0_linux_amd64.tar.gz
+chmod a+x kustomize
+sudo mv kustomize /usr/local/bin/kustomize
