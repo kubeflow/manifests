d16efe7de94e3e441f6666e1def37de72dd90bae
Anna Jung (VMware) <antheaj@vmware.com>
2023-07-14 16:20:14 -0400

    Update doc and tests to use istio 1.17.3
    Signed-off-by: Anna Jung (VMware) <antheaj@vmware.com>

--- tests/gh-actions/install_knative.sh
+++ tests/gh-actions/install_knative.sh
@@ -6,8 +6,8 @@ kustomize build common/knative/knative-serving/base | kubectl apply -f -
 set -e
 kustomize build common/knative/knative-serving/base | kubectl apply -f -
 
-kustomize build common/istio-1-16/cluster-local-gateway/base | kubectl apply -f -
-kustomize build common/istio-1-16/kubeflow-istio-resources/base | kubectl apply -f -
+kustomize build common/istio-1-17/cluster-local-gateway/base | kubectl apply -f -
+kustomize build common/istio-1-17/kubeflow-istio-resources/base | kubectl apply -f -
 
 kubectl wait --for=condition=Ready pods --all --all-namespaces --timeout 600s
 kubectl patch cm config-domain --patch '{"data":{"example.com":""}}' -n knative-serving
