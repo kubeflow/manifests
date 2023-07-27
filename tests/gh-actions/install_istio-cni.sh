4c9a896701eb05d70c0a1e66771f27a147bc7ac8
juliusvonkohout <45896133+juliusvonkohout@users.noreply.github.com>
2023-07-27 12:16:34 +0200

    add tests for istio-cni

---
+++ tests/gh-actions/install_istio-cni.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+echo "Installing Istio-cni ..."
+cd common/istio-cni-1-17
+kustomize build istio-crds/base | kubectl apply -f -
+kustomize build istio-namespace/base | kubectl apply -f -
+kustomize build istio-install/base | kubectl apply -f -
