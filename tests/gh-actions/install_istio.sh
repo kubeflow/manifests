a87495a0b6d9af5b39fc24f081438546853aafb4
Kimonas Sotirchos <kimwnasptd@arrikto.com>
2023-08-16 22:59:03 +0300

    Update Katib tests for MySQL issue with GH Runner (#2508)
    * Update apparmor for mysql
    
    We'll need to update the apparmor profiles in the GH runner host to be
    able to run mysql.
    
    https://kind.sigs.k8s.io/docs/user/known-issues/#apparmor
    
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@gmail.com>
    
    * Wait only for relevant pods
    
    Instead of waiting for all pods in all namespaces we'll only wait for
    the pods the test is creating. This includes:
    * Istio Pods, waited by the script that installs Istio
    * Cert Manager Pods, waited by the script that install Cert Manager
    * Kubeflow Pods
    
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@gmail.com>
    
    * Wait for Experiment to become Running
    
    We saw a race were the test would first try to wait for a Trial object
    when it wasn't yet created.
    
    To mitigate this we'll explicitly make the test wait for the Experiment
    to become Running, before checking the underlying Trials.
    
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@gmail.com>
    
    * Wait 10mins for TFJob to succeed
    
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@gmail.com>
    
    * Make Katib tests run when the istio script changes
    
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@gmail.com>
    
    ---------
    
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@gmail.com>

--- tests/gh-actions/install_istio.sh
+++ tests/gh-actions/install_istio.sh
@@ -5,3 +5,6 @@ cd common/istio-1-17
 kustomize build istio-crds/base | kubectl apply -f -
 kustomize build istio-namespace/base | kubectl apply -f -
 kustomize build istio-install/base | kubectl apply -f -
+
+echo "Waiting for all Istio Pods to become ready..."
+kubectl wait --for=condition=Ready pods --all -n istio-system --timeout 300s
