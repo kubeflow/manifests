bc1eddbb28ba15bcca0a762bc73260acb9f30869
Krzysztof Romanowski <krzysztof.romanowski.kr3@roche.com>
2023-12-14 13:53:55 +0000

    extract namespace creation from install_pipelines.sh script
    Signed-off-by: Krzysztof Romanowski <krzysztof.romanowski.kr3@roche.com>

--- tests/gh-actions/install_pipelines.sh
+++ tests/gh-actions/install_pipelines.sh
@@ -2,10 +2,17 @@
 set -euo pipefail
 echo "Installing Pipelines ..."
 cd apps/pipeline/upstream
-kubectl create ns kubeflow
 kubectl apply -f third-party/metacontroller/base/crd.yaml
 echo "Waiting for crd/compositecontrollers.metacontroller.k8s.io to be available ..."
 kubectl wait --for condition=established --timeout=30s crd/compositecontrollers.metacontroller.k8s.io
 kustomize build env/cert-manager/platform-agnostic-multi-user | kubectl apply -f -
 sleep 60
 kubectl wait --for=condition=Ready pods --all --all-namespaces --timeout 600s
+cd -
+
+echo "Installing Profiles Controller"
+kustomize build apps/profiles/upstream/overlays/kubeflow | kubectl apply -f -
+kubectl -n kubeflow wait --for=condition=Ready pods -l kustomize.component=profiles --timeout 180s
+
+echo "Installing Multitenancy Kubeflow Roles"
+kustomize build common/kubeflow-roles/base | kubectl apply -f -
