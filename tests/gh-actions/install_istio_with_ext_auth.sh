097b6d0523afc4d4fb2f30490f48e1c280e78fe4
Krzysztof Romanowski <krzysztof.romanowski.kr3@roche.com>
2023-12-14 13:44:19 +0000

    create test for running kf pipeline with m2m on kind
    Signed-off-by: Krzysztof Romanowski <krzysztof.romanowski.kr3@roche.com>

---
+++ tests/gh-actions/install_istio_with_ext_auth.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+echo "Installing Istio configured with external authorization..."
+cd common/istio-1-17
+kustomize build istio-crds/base | kubectl apply -f -
+kustomize build istio-namespace/base | kubectl apply -f -
+kustomize build istio-install/overlays/oauth2-proxy | kubectl apply -f -
+cd -
+
+echo "Waiting for all Istio Pods to become ready..."
+kubectl wait --for=condition=Ready pods --all -n istio-system --timeout 300s
+
+echo "Installing oauth2-proxy..."
+cd common/oidc-client
+kustomize build oauth2-proxy/overlays/m2m-self-signed/ | kubectl apply -f -
+kubectl wait --for=condition=ready pod -l 'app.kubernetes.io/name=oauth2-proxy' --timeout=180s -n oauth2-proxy
