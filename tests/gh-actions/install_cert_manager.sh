db90a315cfe3895ef6ec4159585f0efa83448c8c
Krzysztof Romanowski <krzysztof.romanowski.kr3@roche.com>
2023-12-31 12:31:49 +0000

    add kubectl wait for cert-manager endpoints
    Signed-off-by: Krzysztof Romanowski <krzysztof.romanowski.kr3@roche.com>

--- tests/gh-actions/install_cert_manager.sh
+++ tests/gh-actions/install_cert_manager.sh
@@ -4,6 +4,10 @@ echo "Installing cert-manager ..."
 cd common/cert-manager
 kubectl create namespace cert-manager
 kustomize build cert-manager/base | kubectl apply -f -
+
 echo "Waiting for cert-manager to be ready ..."
 kubectl wait --for=condition=ready pod -l 'app in (cert-manager,webhook)' --timeout=180s -n cert-manager
+kubectl wait --for=jsonpath='{.subsets[0].addresses[0].targetRef.kind}'=Pod endpoints -l 'app in (cert-manager,webhook)' --timeout=180s -n cert-manager
+
+echo "Deploy clusterissuer.cert-manager.io/kubeflow-self-signing-issuer"
 kustomize build kubeflow-issuer/base | kubectl apply -f -
