0d53c9ee960430a4bc002116d97bf036a145c923
Julius von Kohout <45896133+juliusvonkohout@users.noreply.github.com>
2024-02-28 19:34:57 +0100

    Update install_cert_manager.sh
    Needed for https://github.com/kubeflow/manifests/pull/2623

--- tests/gh-actions/install_cert_manager.sh
+++ tests/gh-actions/install_cert_manager.sh
@@ -4,6 +4,9 @@ echo "Installing cert-manager ..."
 cd common/cert-manager
 kubectl create namespace cert-manager
 kustomize build cert-manager/base | kubectl apply -f -
+
 echo "Waiting for cert-manager to be ready ..."
 kubectl wait --for=condition=ready pod -l 'app in (cert-manager,webhook)' --timeout=180s -n cert-manager
+kubectl wait --for=jsonpath='{.subsets[0].addresses[0].targetRef.kind}'=Pod endpoints -l 'app in (cert-manager,webhook)' --timeout=180s -n cert-manager
+
 kustomize build kubeflow-issuer/base | kubectl apply -f -
