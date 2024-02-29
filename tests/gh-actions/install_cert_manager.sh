d49f63cdcf6da86afcaa7e20ff87e64499766adb
Ricardo Martinelli de Oliveira <rmartine@redhat.com>
2024-02-29 07:41:34 -0300

    Update kserve manifests from v0.11.2 (#2627)
    * Update kubeflow/kubeflow manifests from v1.8.0 (#2567)
    
    Signed-off-by: juliusvonkohout <45896133+juliusvonkohout@users.noreply.github.com>
    
    * Update kserve manifests from v0.11.2
    
    Update kserve/kserve manifests from v0.11.2
    
    Signed-off-by: Ricardo M. Oliveira <rmartine@redhat.com>
    Signed-off-by: juliusvonkohout <45896133+juliusvonkohout@users.noreply.github.com>
    
    * Update install_cert_manager.sh
    
    Signed-off-by: juliusvonkohout <45896133+juliusvonkohout@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: juliusvonkohout <45896133+juliusvonkohout@users.noreply.github.com>
    Signed-off-by: Ricardo M. Oliveira <rmartine@redhat.com>
    Co-authored-by: Daniela Plascencia <daniela.plascencia@canonical.com>
    Co-authored-by: Julius von Kohout <45896133+juliusvonkohout@users.noreply.github.com>

--- tests/gh-actions/install_cert_manager.sh
+++ tests/gh-actions/install_cert_manager.sh
@@ -9,4 +9,5 @@ echo "Waiting for cert-manager to be ready ..."
 kubectl wait --for=condition=ready pod -l 'app in (cert-manager,webhook)' --timeout=180s -n cert-manager
 kubectl wait --for=jsonpath='{.subsets[0].addresses[0].targetRef.kind}'=Pod endpoints -l 'app in (cert-manager,webhook)' --timeout=180s -n cert-manager
 
+echo "Deploy clusterissuer.cert-manager.io/kubeflow-self-signing-issuer"
 kustomize build kubeflow-issuer/base | kubectl apply -f -
