457e35280d3de48bd10048ed15b82694bbd91ec1
biswajit-9776 <biswajitpatt139@gmail.com>
2024-05-13 17:23:59 +0530

    Update istio to v1.22.0
    Signed-off-by: biswajit-9776 <biswajitpatt139@gmail.com>

--- tests/gh-actions/install_istio-cni.sh
+++ tests/gh-actions/install_istio-cni.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 echo "Installing Istio-cni ..."
-cd common/istio-cni-1-17
+cd common/istio-cni-1-22
 kustomize build istio-crds/base | kubectl apply -f -
 kustomize build istio-namespace/base | kubectl apply -f -
 kustomize build istio-install/base | kubectl apply -f -
--- tests/gh-actions/install_istio.sh
+++ tests/gh-actions/install_istio.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 echo "Installing Istio ..."
-cd common/istio-1-17
+cd common/istio-1-22
 kustomize build istio-crds/base | kubectl apply -f -
 kustomize build istio-namespace/base | kubectl apply -f -
 kustomize build istio-install/base | kubectl apply -f -
