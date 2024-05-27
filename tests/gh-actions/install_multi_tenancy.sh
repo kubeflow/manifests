823fb267a66819222b3f6a047cd9f1b606bbbad5
biswajit-9776 <biswajitpatt139@gmail.com>
2024-05-27 19:42:06 +0530

    Adding sleep 30 to multi_tenancy.sh test
    Signed-off-by: biswajit-9776 <biswajitpatt139@gmail.com>

--- tests/gh-actions/install_multi_tenancy.sh
+++ tests/gh-actions/install_multi_tenancy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-set -euo pipefail
+set -euxo pipefail
 
 echo "Installing Profiles Controller"
 kustomize build apps/profiles/upstream/overlays/kubeflow | kubectl apply -f -
+sleep 30
 kubectl -n kubeflow wait --for=condition=Ready pods -l kustomize.component=profiles --timeout 180s
 
 echo "Installing Multitenancy Kubeflow Roles"
