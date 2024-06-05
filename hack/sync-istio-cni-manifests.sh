44fac250e9dcee728ef600d6c912b7a86612b083
biswajit-9776 <biswajitpatt139@gmail.com>
2024-06-05 17:23:42 +0530

    Upgraded istio-cni to v1.21.3
    Signed-off-by: biswajit-9776 <biswajitpatt139@gmail.com>

--- hack/sync-istio-cni-manifests.sh
+++ hack/sync-istio-cni-manifests.sh
@@ -23,7 +23,63 @@ SRC_DIR=${SRC_DIR:=/tmp/istio-cni}
 BRANCH=${BRANCH:=istio-${COMMIT?}}
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-MANIFESTS_DIR=$(dirname $SCRIPT_DIR)
+MANIFESTS_DIR=$(dirname $SCRIPT_DIR)echo "Creating branch: ${BRANCH}"
+
+if [ -n "$(git status --porcelain)" ]; then
+  echo "WARNING: You have uncommitted changes"
+fi
+if [ `git branch --list $BRANCH` ]
+then
+   echo "WARNING: Branch $BRANCH already exists."
+fi
+
+# Create the branch in the manifests repository
+if ! git show-ref --verify --quiet refs/heads/$BRANCH; then
+    git checkout -b $BRANCH
+else
+    echo "Branch $BRANCH already exists."
+fi
+
+if [ ! -d "$ISTIO_NEW" ]; then
+cp -a $ISTIO_OLD $ISTIO_NEW
+fi 
+
+echo "Checking out in $SRC_DIR to $COMMIT..."
+# Checkout the istio repository
+if [ ! -d "$SRC_DIR" ]; then
+mkdir -p $SRC_DIR
+fi
+cd $SRC_DIR
+if [ ! -d "istio-${COMMIT}-linux-amd64.tar.gz" ]; then
+    wget "https://github.com/istio/istio/releases/download/${COMMIT}/istio-${COMMIT}-linux-amd64.tar.gz"
+    tar xvfz istio-${COMMIT}-linux-amd64.tar.gz
+fi
+
+sudo mv istio-${COMMIT}/bin/istioctl /usr/local/bin/istioctl
+
+cd $ISTIO_NEW
+istioctl profile dump default > profile.yaml
+
+# cd $ISTIO_NEW
+# export PATH="$MANIFESTS_DIR/scripts:$PATH"
+istioctl manifest generate --cluster-specific -f profile.yaml -f profile-overlay.yaml > dump.yaml
+./split-istio-packages -f dump.yaml
+mv $ISTIO_NEW/crd.yaml $ISTIO_NEW/istio-crds/base
+mv $ISTIO_NEW/install.yaml $ISTIO_NEW/istio-install/base
+mv $ISTIO_NEW/cluster-local-gateway.yaml $ISTIO_NEW/cluster-local-gateway/base
+rm dump.yaml
+
+if [ -n "$(git status --porcelain)" ]; then
+  echo "WARNING: You have uncommitted changes"
+fi
+
+find "$MANIFESTS_DIR" -type f -exec sed -i "s/istio-cni-${CURRENT_VERSION}/istio-cni-${NEW_VERSION}/g" {} +
+
+echo "Committing the changes..."
+cd "$MANIFESTS_DIR"
+rm -rf $ISTIO_OLD
+git add .
+git commit -s -m "Updated istio-cni to v.${COMMIT}"
 
 ISTIO_OLD=$MANIFESTS_DIR/common/istio-cni-${CURRENT_VERSION}
 ISTIO_NEW=$MANIFESTS_DIR/common/istio-cni-${NEW_VERSION}
@@ -78,10 +134,10 @@ if [ -n "$(git status --porcelain)" ]; then
   echo "WARNING: You have uncommitted changes"
 fi
 
-# find "$MANIFESTS_DIR" -type f -exec sed -i "s/istio-cni-${CURRENT_VERSION}/istio-cni-${NEW_VERSION}/g" {} +
+find "$MANIFESTS_DIR" -type f -exec sed -i "s/istio-cni-${CURRENT_VERSION}/istio-cni-${NEW_VERSION}/g" {} +
 
-# echo "Committing the changes..."
-# cd "$MANIFESTS_DIR"
-# git rm -r $ISTIO_OLD
-# git add .
-# git commit -s -m "Updated istio-cni to v.${COMMIT}"
+echo "Committing the changes..."
+cd "$MANIFESTS_DIR"
+rm -rf $ISTIO_OLD
+git add .
+git commit -s -m "Updated istio-cni to v.${COMMIT}"
--- tests/gh-actions/install_istio-cni.sh
+++ tests/gh-actions/install_istio-cni.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 echo "Installing Istio-cni ..."
-cd common/istio-cni-1-20
+cd common/istio-cni-1-21
 kustomize build istio-crds/base | kubectl apply -f -
 kustomize build istio-namespace/base | kubectl apply -f -
 kustomize build istio-install/base | kubectl apply -f -
