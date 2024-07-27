ffce2c721c094dbcc1940eef614e8931f945e229
hansinikarunarathne <107214435+hansinikarunarathne@users.noreply.github.com>
2024-07-27 14:43:03 +0530

    Change namespace of calico
    Signed-off-by: hansinikarunarathne <107214435+hansinikarunarathne@users.noreply.github.com>
    Signed-off-by: hansinikarunarathne <hansini.20@cse.mrt.ac.lk>

--- tests/gh-actions/install_calico.sh
+++ tests/gh-actions/install_calico.sh
@@ -3,4 +3,4 @@ set -e
 kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.0/manifests/tigera-operator.yaml
 kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.0/manifests/custom-resources.yaml
 
-kubectl wait --for=condition=Ready pods --all --namespace=calico-system --timeout 300s
+kubectl wait --for=condition=Ready pods --all --namespace=tigera-operator --timeout 300s
