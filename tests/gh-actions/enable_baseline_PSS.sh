74daa4e910ff09c755ea567a85d724771efa5d1d
biswajit-9776 <biswajitpatt139@gmail.com>
2024-07-30 20:43:56 +0530

    Added PSS scripts for both baseline and restricted labels of static namespaces and renamed directories
    Signed-off-by: biswajit-9776 <biswajitpatt139@gmail.com>

--- tests/gh-actions/enable_baseline_PSS.sh
+++ tests/gh-actions/enable_baseline_PSS.sh
@@ -1,33 +1,10 @@
 #!/bin/bash
-set -euo pipefail
 
-ISTIO_SYSTEM="istio-system"
-AUTH="auth"
-CERT_MANAGER="cert-manager"
-OAUTH2_PROXY="oauth2-proxy"
-KUBEFLOW="kubeflow"
+NAMESPACES=("istio-system" "auth" "cert-manager" "oauth2-proxy" "kubeflow")
 
-if kubectl get namespace "$ISTIO_SYSTEM" >/dev/null 2>&1; then
-    echo "Patching the PSS-baseline labels for namespace $ISTIO_SYSTEM..."
-    kubectl patch namespace $ISTIO_SYSTEM --patch-file ./contrib/security/PSS/static/baseline/patches/istio-labels.yaml
-fi
-
-if kubectl get namespace "$AUTH" >/dev/null 2>&1; then
-    echo "Patching the PSS-baseline labels for namespace $AUTH..."
-    kubectl patch namespace $AUTH --patch-file ./contrib/security/PSS/static/baseline/patches/dex-labels.yaml
-fi
-
-if kubectl get namespace "$CERT_MANAGER" >/dev/null 2>&1; then
-    echo "Patching the PSS-baseline labels for namespace $CERT_MANAGER..."
-    kubectl patch namespace $CERT_MANAGER --patch-file ./contrib/security/PSS/static/baseline/patches/cert-manager-labels.yaml
-fi
-
-if kubectl get namespace "$OAUTH2_PROXY" >/dev/null 2>&1; then
-    echo "Patching the PSS-baseline labels for namespace $OAUTH2_PROXY..."
-    kubectl patch namespace $OAUTH2_PROXY --patch-file ./contrib/security/PSS/static/baseline/patches/oauth2-proxy-labels.yaml
-fi
-
-if kubectl get namespace "$KUBEFLOW" >/dev/null 2>&1; then
-    echo "Patching the PSS-baseline labels for namespace $KUBEFLOW..."
-    kubectl patch namespace $KUBEFLOW --patch-file ./contrib/security/PSS/static/baseline/patches/kubeflow-labels.yaml
-fi
+for NAMESPACE in "${NAMESPACES[@]}"; do
+    if kubectl get namespace "$NAMESPACE" >/dev/null 2>&1; then
+        echo "Patching the PSS-baseline labels for namespace $NAMESPACE..."
+        kubectl patch namespace $NAMESPACE --patch-file ./contrib/security/PSS/static/baseline/patches/${NAMESPACE}-labels.yaml
+    fi
+done
