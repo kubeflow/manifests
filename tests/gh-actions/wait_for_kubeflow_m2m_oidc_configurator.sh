6bc14a98ada514f3f737ff8dbfbaa7629acb91e8
biswajit-9776 <biswajitpatt139@gmail.com>
2024-08-21 20:50:02 +0530

    Limited script to run for a minute
    Signed-off-by: biswajit-9776 <biswajitpatt139@gmail.com>

--- tests/gh-actions/wait_for_kubeflow_m2m_oidc_configurator.sh
+++ tests/gh-actions/wait_for_kubeflow_m2m_oidc_configurator.sh
@@ -2,6 +2,7 @@
 
 CRONJOB_NAME=kubeflow-m2m-oidc-configurator
 NAMESPACE=istio-system
+SECONDS=0
 
 # Function to get the latest Job created by the CronJob
 get_latest_job() {
@@ -13,7 +14,7 @@ get_latest_job() {
 
 # Wait until a Job is created
 echo "Waiting for a Job to be created by the ${CRONJOB_NAME} CronJob..."
-while true; do
+while [$SECONDS -lt 60]; do
   JOB_NAME=$(get_latest_job)
   if [[ -n "${JOB_NAME}" ]]; then
     echo "Job ${JOB_NAME} created."
@@ -25,7 +26,7 @@ done
 
 # Wait for the Job to complete successfully
 echo "Waiting for the Job ${JOB_NAME} to complete..."
-while true; do
+while [$SECONDS -lt 60]; do
   STATUS=$(kubectl get job "${JOB_NAME}" -n "${NAMESPACE}" -o jsonpath='{.status.conditions[?(@.type=="Complete")].status}')
   if [[ "${STATUS}" == "True" ]]; then
     echo "Job ${JOB_NAME} completed successfully."
