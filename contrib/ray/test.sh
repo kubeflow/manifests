d4060285b3e6f4ad2ac7b55a070eac88d0513ecc
hansinikarunarathne <hansini.20@cse.mrt.ac.lk>
2024-09-08 12:42:21 +0530

    Add istio authorization policy for ray_cluster
    Signed-off-by: hansinikarunarathne <hansini.20@cse.mrt.ac.lk>

--- contrib/ray/test.sh
+++ contrib/ray/test.sh
@@ -7,62 +7,40 @@ TIMEOUT=120  # timeout in seconds
 SLEEP_INTERVAL=30  # interval between checks in seconds
 RAY_VERSION=2.23.0
 
-function trap_handler {
-  kill $PID
-  # Delete RayCluster
-  kubectl -n $NAMESPACE delete -f raycluster_example.yaml
+# function trap_handler {
+#   kill $PID
+#   # Delete RayCluster
+#   kubectl -n $NAMESPACE delete -f raycluster_example.yaml
 
-  # Wait for all Ray Pods to be deleted.
-  start_time=$(date +%s)
-  while true; do
-    pods=$(kubectl -n $NAMESPACE get pods -o json | jq '.items | length')
-    if [ "$pods" -eq 1 ]; then
-      break
-    fi
-    current_time=$(date +%s)
-    elapsed_time=$((current_time - start_time))
-    if [ "$elapsed_time" -ge "$TIMEOUT" ]; then
-      echo "Timeout exceeded. Exiting loop."
-      exit 1
-    fi
-    sleep $SLEEP_INTERVAL
-  done
+#   # Wait for all Ray Pods to be deleted.
+#   start_time=$(date +%s)
+#   while true; do
+#     pods=$(kubectl -n $NAMESPACE get pods -o json | jq '.items | length')
+#     if [ "$pods" -eq 1 ]; then
+#       break
+#     fi
+#     current_time=$(date +%s)
+#     elapsed_time=$((current_time - start_time))
+#     if [ "$elapsed_time" -ge "$TIMEOUT" ]; then
+#       echo "Timeout exceeded. Exiting loop."
+#       exit 1
+#     fi
+#     sleep $SLEEP_INTERVAL
+#   done
 
-  # Delete KubeRay operator
-  kustomize build kuberay-operator/base | kubectl -n $NAMESPACE delete -f -
-}
+#   # Delete KubeRay operator
+#   kustomize build kuberay-operator/base | kubectl -n $NAMESPACE delete -f -
 
-trap trap_handler EXIT
+# }
 
-# Download Istioctl and its manifests.
-export ISTIO_VERSION=1.21.1
-curl -L https://istio.io/downloadIstio | sh -
-cd istio-1.21.1
-export PATH=$PWD/bin:$PATH
+# trap trap_handler EXIT
 
-kubectl create namespace $NAMESPACE
+# kubectl create namespace $NAMESPACE
 
-# Install Istio with:
-#   1. 100% trace sampling for demo purposes.
-#   2. "sanitize_te" disabled for proper gRPC interception. This is required by Istio 1.21.0 (https://github.com/istio/istio/issues/49685).
-#   3. TLS 1.3 enabled.
-istioctl install -y -f - <<EOF
-apiVersion: install.istio.io/v1alpha1
-kind: IstioOperator
-metadata:
-  namespace: kubeflow
-spec:
-  meshConfig:
-    defaultConfig:
-      tracing:
-        sampling: 100
-      runtimeValues:
-        envoy.reloadable_features.sanitize_te: "false"
-    meshMTLS:
-      minProtocolVersion: TLSV1_3
-EOF
-
-cd ..
+# Check if namespace exists, if not, create it
+if ! kubectl get namespace $NAMESPACE > /dev/null 2>&1; then
+  kubectl create namespace $NAMESPACE
+fi
 
 kubectl label namespace kubeflow istio-injection=enabled
 
@@ -77,10 +55,13 @@ kubectl -n $NAMESPACE wait --for=condition=available --timeout=600s deploy/kuber
 kubectl -n $NAMESPACE get pod -l app.kubernetes.io/component=kuberay-operator
 
 # Create a RayCluster Headless serivice
-kubectl -n $NAMESPACE apply -f raycluster_istio_headless_svc.yaml
+kubectl apply -f raycluster_istio_headless_svc.yaml
+
+# Create a RayCluster AuthorizationPolicy
+kubectl apply -f raycluster_istio_auth_policy.yaml
 
 # Create a RayCluster custom resource.
-kubectl -n $NAMESPACE apply -f raycluster_example.yaml
+kubectl  apply -f raycluster_example.yaml
 
 # Wait for the RayCluster to be ready.
 sleep 5
