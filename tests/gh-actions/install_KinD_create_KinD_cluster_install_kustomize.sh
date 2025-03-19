aad6ad2d442bcf0fd347cb2482f87e6a56d0d38f
Julius von Kohout <45896133+juliusvonkohout@users.noreply.github.com>
2025-03-19 16:06:46 +0100

    Update Kind and test Kubernetes 1.29-1.32 (#3059)
    * Update install_KinD_create_KinD_cluster_install_kustomize.sh
    
    Signed-off-by: Julius von Kohout <45896133+juliusvonkohout@users.noreply.github.com>
    
    * Update install_KinD_create_KinD_cluster_install_kustomize.sh
    
    Signed-off-by: Julius von Kohout <45896133+juliusvonkohout@users.noreply.github.com>
    
    * Update README.md
    
    Signed-off-by: Julius von Kohout <45896133+juliusvonkohout@users.noreply.github.com>
    
    * Update install_KinD_create_KinD_cluster_install_kustomize.sh
    
    Signed-off-by: Julius von Kohout <45896133+juliusvonkohout@users.noreply.github.com>
    
    * Update install_KinD_create_KinD_cluster_install_kustomize.sh
    
    Signed-off-by: Julius von Kohout <45896133+juliusvonkohout@users.noreply.github.com>
    
    * Update install.yaml
    
    Signed-off-by: Julius von Kohout <45896133+juliusvonkohout@users.noreply.github.com>
    
    * Update install_KinD_create_KinD_cluster_install_kustomize.sh
    
    Signed-off-by: Julius von Kohout <45896133+juliusvonkohout@users.noreply.github.com>
    
    * Update install_KinD_create_KinD_cluster_install_kustomize.sh
    
    Signed-off-by: Julius von Kohout <45896133+juliusvonkohout@users.noreply.github.com>
    
    * Update install.yaml
    
    Signed-off-by: Julius von Kohout <45896133+juliusvonkohout@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Julius von Kohout <45896133+juliusvonkohout@users.noreply.github.com>

--- tests/gh-actions/install_KinD_create_KinD_cluster_install_kustomize.sh
+++ tests/gh-actions/install_KinD_create_KinD_cluster_install_kustomize.sh
@@ -20,7 +20,7 @@ if [ -e /swapfile ]; then
 fi
 
 {
-    curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.26.0/kind-linux-amd64
+    curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.27.0/kind-linux-amd64
     chmod +x ./kind
     sudo mv kind /usr/local/bin
 } || { echo "Failed to install KinD"; exit 1; }
@@ -49,11 +49,11 @@ kubeadmConfigPatches:
         \"service-account-signing-key-file\": \"/etc/kubernetes/pki/sa.key\"
 nodes:
 - role: control-plane
-  image: kindest/node:v1.32.0@sha256:c48c62eac5da28cdadcf560d1d8616cfa6783b58f0d94cf63ad1bf49600cb027
+  image: kindest/node:v1.32.2@sha256:f226345927d7e348497136874b6d207e0b32cc52154ad8323129352923a3142f
 - role: worker
-  image: kindest/node:v1.32.0@sha256:c48c62eac5da28cdadcf560d1d8616cfa6783b58f0d94cf63ad1bf49600cb027
+  image: kindest/node:v1.32.2@sha256:f226345927d7e348497136874b6d207e0b32cc52154ad8323129352923a3142f
 - role: worker
-  image: kindest/node:v1.32.0@sha256:c48c62eac5da28cdadcf560d1d8616cfa6783b58f0d94cf63ad1bf49600cb027
+  image: kindest/node:v1.32.2@sha256:f226345927d7e348497136874b6d207e0b32cc52154ad8323129352923a3142f
 " | kind create cluster --config -
 
 
