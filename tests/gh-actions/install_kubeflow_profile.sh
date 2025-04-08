9d000044a0cfdc204b07ec0c4cc58c0dc95e422d
Julius von Kohout <45896133+juliusvonkohout@users.noreply.github.com>
2025-04-08 20:03:14 +0200

    Update install_kubeflow_profile.sh
    Signed-off-by: Julius von Kohout <45896133+juliusvonkohout@users.noreply.github.com>

--- tests/gh-actions/install_kubeflow_profile.sh
+++ tests/gh-actions/install_kubeflow_profile.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 set -euxo
-
 kustomize build common/user-namespace/base | kubectl apply -f -
-sleep 30
-
+sleep 30 # Let the profile controler reconcile the namespace
 PROFILE_CONTROLLER_POD=$(kubectl get pods -n kubeflow -o json | jq -r '.items[] | select(.metadata.name | startswith("profiles-deployment")) | .metadata.name')
-if [[ -z "$PROFILE_CONTROLLER_POD" ]]; then
-  echo "Error: profiles-deployment pod not found in kubeflow namespace."
-  exit 1
-fi
-
 kubectl logs -n kubeflow "$PROFILE_CONTROLLER_POD"
-
 KF_PROFILE=kubeflow-user-example-com
-kubectl -n $KF_PROFILE get pods,configmaps,secrets 
+kubectl -n $KF_PROFILE get pods,configmaps,secrets
