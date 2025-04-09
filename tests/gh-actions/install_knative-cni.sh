44dbd2967d84c9540ba4c0972da1b9983088354c
Kunal Dugar <yoyokvunal@gmail.com>
2025-04-09 21:10:21 +0530

    Updated the gha workflows to use new install_katib, made changes as s… (#3092)
    * updated the gha workflows to use new install_katib, made changes as suggested
    
    Signed-off-by: kunal-511 <yoyokvunal@gmail.com>
    
    * Fixed the namespace error
    
    Signed-off-by: kunal-511 <yoyokvunal@gmail.com>
    
    * Update full_kubeflow_integration_test.yaml
    
    Signed-off-by: Julius von Kohout <45896133+juliusvonkohout@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: kunal-511 <yoyokvunal@gmail.com>
    Signed-off-by: Julius von Kohout <45896133+juliusvonkohout@users.noreply.github.com>
    Co-authored-by: Julius von Kohout <45896133+juliusvonkohout@users.noreply.github.com>

--- tests/gh-actions/install_knative-cni.sh
+++ tests/gh-actions/install_knative-cni.sh
@@ -17,6 +17,10 @@ set -e
 kustomize build common/istio-cni-1-24/cluster-local-gateway/base | kubectl apply -f -
 kustomize build common/istio-cni-1-24/kubeflow-istio-resources/base | kubectl apply -f -
 
-kubectl wait --for=condition=Ready pods --all --all-namespaces --timeout=300s \
-  --field-selector=status.phase!=Succeeded
+kubectl wait --for=condition=Ready pods --all --all-namespaces --timeout=300s --field-selector=status.phase!=Succeeded
+kubectl wait --for=condition=Available deployment/activator -n knative-serving --timeout=10s
+kubectl wait --for=condition=Available deployment/autoscaler -n knative-serving --timeout=10s
+kubectl wait --for=condition=Available deployment/controller -n knative-serving --timeout=10s
+kubectl wait --for=condition=Available deployment/webhook -n knative-serving --timeout=10s
 kubectl patch cm config-domain --patch '{"data":{"example.com":""}}' -n knative-serving
+kubectl get deployment -n knative-serving
