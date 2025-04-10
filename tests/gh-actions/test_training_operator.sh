8ff3e662e859cf9065b45aa5fdd42f4f77b2afe6
kunal-511 <yoyokvunal@gmail.com>
2025-04-10 21:18:06 +0530

    Fixed the namespace error
    Signed-off-by: kunal-511 <yoyokvunal@gmail.com>

--- tests/gh-actions/test_training_operator.sh
+++ tests/gh-actions/test_training_operator.sh
@@ -3,17 +3,14 @@ set -euxo
 
 KF_PROFILE=${1:-kubeflow-user-example-com}
 
-for i in {1..30}; do
-  kubectl api-resources | grep -q "pytorchjobs.*kubeflow" && break
-  sleep 5
-done
+cat tests/gh-actions/kf-objects/training_operator_job.yaml | \
+  sed 's/name: pytorch-simple/name: pytorch-simple\n  namespace: '"$KF_PROFILE"'/g' > /tmp/pytorch-job.yaml
 
-JOB_YAML=$(sed 's/namespace: .*/namespace: '"$KF_PROFILE"'/g' tests/gh-actions/kf-objects/training_operator_job.yaml)
-echo "$JOB_YAML" | kubectl apply -f -
 
-sleep 30
+kubectl apply -f /tmp/pytorch-job.yaml
+
+sleep 60
 
 kubectl get pytorchjob/pytorch-simple -n $KF_PROFILE
-
 
 kubectl wait --for=condition=Succeeded pytorchjob/pytorch-simple -n $KF_PROFILE --timeout=600s 
