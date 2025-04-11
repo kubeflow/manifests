d484a70017cec88d699f910524eea2a0bd9f142e
kunal-511 <yoyokvunal@gmail.com>
2025-04-11 22:16:25 +0530

    Fixed deployment name
    Signed-off-by: kunal-511 <yoyokvunal@gmail.com>

--- tests/gh-actions/install_volumes_web_app.sh
+++ tests/gh-actions/install_volumes_web_app.sh
@@ -6,4 +6,4 @@ cd apps/volumes-web-app/upstream
 kustomize build overlays/istio | kubectl apply -f -
 cd ../../../
 
-kubectl wait --for=condition=Available deployment/volumes-web-app -n kubeflow --timeout=180s 
+kubectl wait --for=condition=Available deployment/volumes-web-app-deployment -n kubeflow --timeout=180s 
