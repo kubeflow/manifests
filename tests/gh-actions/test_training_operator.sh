c0d31bebfff7df2f7ed266089021a0ee69c2c91f
kunal-511 <yoyokvunal@gmail.com>
2025-04-11 11:13:39 +0530

    Fixed the unauth token probelm
    Signed-off-by: kunal-511 <yoyokvunal@gmail.com>

--- tests/gh-actions/test_training_operator.sh
+++ tests/gh-actions/test_training_operator.sh
@@ -6,11 +6,12 @@ KF_PROFILE=${1:-kubeflow-user-example-com}
 cat tests/gh-actions/kf-objects/training_operator_job.yaml | \
   sed 's/name: pytorch-simple/name: pytorch-simple\n  namespace: '"$KF_PROFILE"'/g' > /tmp/pytorch-job.yaml
 
-
 kubectl apply -f /tmp/pytorch-job.yaml
 
-sleep 60
+sleep 90
 
 kubectl get pytorchjob/pytorch-simple -n $KF_PROFILE
 
-kubectl wait --for=condition=Succeeded pytorchjob/pytorch-simple -n $KF_PROFILE --timeout=300s 
+kubectl wait --for=condition=Running pytorchjob/pytorch-simple -n $KF_PROFILE --timeout=180s
+
+kubectl wait --for=condition=Succeeded pytorchjob/pytorch-simple -n $KF_PROFILE --timeout=450s 
