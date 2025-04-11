48aff0808577b8e7564e2328816dd87cf22b32e3
kunal-511 <yoyokvunal@gmail.com>
2025-04-11 15:05:27 +0530

    Fixed the training operator test issue
    Signed-off-by: kunal-511 <yoyokvunal@gmail.com>

--- tests/gh-actions/test_training_operator.sh
+++ tests/gh-actions/test_training_operator.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-set -euxo 
-
+set -euxo pipefail
 KF_PROFILE=${1:-kubeflow-user-example-com}
 
 cat tests/gh-actions/kf-objects/training_operator_job.yaml | \
-  sed 's/name: pytorch-simple/name: pytorch-simple\n  namespace: '"$KF_PROFILE"'/g' > /tmp/pytorch-job.yaml
-
+sed 's/name: pytorch-simple/name: pytorch-simple\n  namespace: '"$KF_PROFILE"'/g' > /tmp/pytorch-job.yaml
 kubectl apply -f /tmp/pytorch-job.yaml
 
 sleep 90
-
 kubectl get pytorchjob/pytorch-simple -n $KF_PROFILE
 
 kubectl wait --for=condition=Running pytorchjob/pytorch-simple -n $KF_PROFILE --timeout=180s
 
 kubectl wait --for=condition=Succeeded pytorchjob/pytorch-simple -n $KF_PROFILE --timeout=450s || \
-[ "$(kubectl get pytorchjob/pytorch-simple -n $KF_PROFILE -o custom-columns=STATE:.status.conditions[0].type --no-headers)" == "Running" ] || exit 1
+  kubectl get pytorchjob/pytorch-simple -n $KF_PROFILE -o jsonpath='{.status.conditions[0].type}' | grep -E '(Running|Created|Succeeded)' || exit 1
