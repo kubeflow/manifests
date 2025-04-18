908c1a562b1f556c23fbb100cfa46687748eb6bf
Julius von Kohout <45896133+juliusvonkohout@users.noreply.github.com>
2025-04-18 11:25:08 +0200

    Update install_knative-cni.sh
    Signed-off-by: Julius von Kohout <45896133+juliusvonkohout@users.noreply.github.com>

--- tests/gh-actions/install_knative-cni.sh
+++ tests/gh-actions/install_knative-cni.sh
@@ -5,19 +5,18 @@ echo "Installing KNative with Istio-CNI ..."
 
 # Retry mechanism for applying Knative manifests
 set +e
-for _ in {1..5}; do
+for ((i=1; i<=3; i++)); do
     if kustomize build common/knative/knative-serving/overlays/gateways | kubectl apply -f -; then
         break
     fi
-    echo "Retrying in 30 seconds..."
-    sleep 30
+    kubectl wait --for=condition=Ready pods --all --all-namespaces --timeout=60s --field-selector=status.phase!=Succeeded
 done
 set -e
 
 kustomize build common/istio-cni-1-24/cluster-local-gateway/base | kubectl apply -f -
 kustomize build common/istio-cni-1-24/kubeflow-istio-resources/base | kubectl apply -f -
 
-kubectl wait --for=condition=Ready pods --all --all-namespaces --timeout=300s --field-selector=status.phase!=Succeeded
+kubectl wait --for=condition=Ready pods --all --all-namespaces --timeout=60s --field-selector=status.phase!=Succeeded
 kubectl wait --for=condition=Available deployment/activator -n knative-serving --timeout=10s
 kubectl wait --for=condition=Available deployment/autoscaler -n knative-serving --timeout=10s
 kubectl wait --for=condition=Available deployment/controller -n knative-serving --timeout=10s
