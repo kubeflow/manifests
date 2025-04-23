460712da4e8e700d77277ca2b7393fe0b4f88d04
kunal-511 <yoyokvunal@gmail.com>
2025-04-23 19:21:46 +0530

    Updated to use restricted patches instead of baseline patches
    Signed-off-by: kunal-511 <yoyokvunal@gmail.com>

--- tests/gh-actions/enable_restricted_PSS.sh
+++ tests/gh-actions/enable_restricted_PSS.sh
@@ -5,9 +5,9 @@ NAMESPACES=("istio-system" "auth" "cert-manager" "oauth2-proxy" "kubeflow")
 for NAMESPACE in "${NAMESPACES[@]}"; do
     if kubectl get namespace "$NAMESPACE" >/dev/null 2>&1; then
         if [ -f "./experimental/security/PSS/static/restricted/patches/${NAMESPACE}-labels.yaml" ]; then
-            PATCH_OUTPUT=$(kubectl patch namespace $NAMESPACE --patch-file ./experimental/security/PSS/static/baseline/patches/${NAMESPACE}-labels.yaml 2>&1)
+            PATCH_OUTPUT=$(kubectl patch namespace $NAMESPACE --patch-file ./experimental/security/PSS/static/restricted/patches/${NAMESPACE}-labels.yaml 2>&1)
             if echo "$PATCH_OUTPUT" | grep -q "violate the new PodSecurity"; then
-                echo "\nWARNING PSS VIOLATED\n"
+                exit 1
             fi
         fi
     fi
