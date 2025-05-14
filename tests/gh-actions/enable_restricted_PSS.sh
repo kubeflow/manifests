533ca9f8c1c97836fae6f5946c76ce31f42dd2c9
Harshvir Potpose <hpotpose62@gmail.com>
2025-05-14 19:23:23 +0530

    Apply pss labels from diectly PSS enable scripts (#3132)
    * apply pss from PSS enable scripts
    
    Signed-off-by: Harshvir Potpose <hpotpose62@gmail.com>
    
    * fix net-istio-controller pss warning
    
    Signed-off-by: Harshvir Potpose <hpotpose62@gmail.com>
    
    ---------
    
    Signed-off-by: Harshvir Potpose <hpotpose62@gmail.com>

--- tests/gh-actions/enable_restricted_PSS.sh
+++ tests/gh-actions/enable_restricted_PSS.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-set -euxo pipefail
+set -euo pipefail
 
 NAMESPACES=("istio-system" "auth" "cert-manager" "oauth2-proxy" "kubeflow" "knative-serving")
 for NAMESPACE in "${NAMESPACES[@]}"; do
     if kubectl get namespace "$NAMESPACE" >/dev/null 2>&1; then
-        if [ -f "./experimental/security/PSS/static/restricted/patches/${NAMESPACE}-labels.yaml" ]; then
-            PATCH_OUTPUT=$(kubectl patch namespace $NAMESPACE --patch-file ./experimental/security/PSS/static/restricted/patches/${NAMESPACE}-labels.yaml 2>&1)
-            if echo "$PATCH_OUTPUT" | grep -q "violate the new PodSecurity"; then
-                echo "\nWARNING PSS VIOLATED\n"
-            fi
+        PATCH_OUTPUT=$(kubectl label namespace $NAMESPACE pod-security.kubernetes.io/enforce=restricted --overwrite 2>&1)
+        if echo "$PATCH_OUTPUT" | grep -q "violate the new PodSecurity"; then
+            echo "WARNING: PSS violation detected for namespace $NAMESPACE"
+            echo "$PATCH_OUTPUT" | grep -A 5 "violate the new PodSecurity"
+        else
+            echo "✅ Namespace '$NAMESPACE' labeled successfully."
         fi
     fi
 done
