880c28c358b7530748787a3f87402af81fad85eb
kunal-511 <yoyokvunal@gmail.com>
2025-06-03 17:27:18 +0530

    Helm chart support for Kubeflow spark-operator
    Signed-off-by: kunal-511 <yoyokvunal@gmail.com>

---
+++ tests/helm_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+echo "Installing Helm..."
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+rm get_helm.sh
+
+echo "Helm $(helm version --short) installed successfully" 
