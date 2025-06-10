4de47bf475bcf20260b4112e371ad48ede75e5b5
Harshvir Potpose <hpotpose62@gmail.com>
2025-06-10 17:09:04 +0530

    fix port-forward
    Signed-off-by: Harshvir Potpose <hpotpose62@gmail.com>

--- tests/port_forward_gateway.sh
+++ tests/port_forward_gateway.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euxo pipefail
 
-GATEWAY_SERVICE=$(kubectl get svc kubeflow-gateway-istio -n kubeflow -o jsonpath='{.metadata.name}')
-nohup kubectl port-forward -n kubeflow svc/$GATEWAY_SERVICE 8080:80 &
+GATEWAY_SERVICE=$(kubectl get svc kubeflow-gateway-istio -n istio-system -o jsonpath='{.metadata.name}')
+nohup kubectl port-forward -n istio-system svc/$GATEWAY_SERVICE 8080:80 &
 timeout 60s bash -c 'until curl -s localhost:8080 > /dev/null || curl -s -I localhost:8080 | grep -q "HTTP/"; do sleep 5; done' 
