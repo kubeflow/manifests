74670005fb233f2e1ef2ffc36bfc5b37de063f04
Julius von Kohout <45896133+juliusvonkohout@users.noreply.github.com>
2025-06-11 22:56:00 +0200

    Update install_KinD_create_KinD_cluster_install_kustomize.sh
    Signed-off-by: Julius von Kohout <45896133+juliusvonkohout@users.noreply.github.com>

--- tests/install_KinD_create_KinD_cluster_install_kustomize.sh
+++ tests/install_KinD_create_KinD_cluster_install_kustomize.sh
@@ -20,7 +20,7 @@ if [ -e /swapfile ]; then
 fi
 
 {
-    curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.27.0/kind-linux-amd64
+    curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.29.0/kind-linux-amd64
     chmod +x ./kind
     sudo mv kind /usr/local/bin
 } || { echo "Failed to install KinD"; exit 1; }
