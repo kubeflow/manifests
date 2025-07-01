f7b9c8d6a355225bffc39877c76e129c436cf11c
kunal-511 <yoyokvunal@gmail.com>
2025-07-02 00:10:34 +0530

    tests for trainer
    Signed-off-by: kunal-511 <yoyokvunal@gmail.com>

---
+++ tests/trainer_install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -euo pipefail
+
+cd applications/trainer/upstream
+
+kustomize build base/crds | kubectl apply --server-side --force-conflicts -f -
+
+kubectl wait --for condition=NamesAccepted crd/trainjobs.trainer.kubeflow.org --timeout=60s
+kubectl wait --for condition=established crd/trainjobs.trainer.kubeflow.org --timeout=60s
+
+kustomize build overlays/manager | kubectl apply --server-side --force-conflicts -f -
+kubectl wait --for=condition=Available deployment/kubeflow-trainer-controller-manager -n kubeflow-system --timeout=180s
+
+kubectl apply --server-side --force-conflicts -f overlays/kubeflow-platform/kubeflow-trainer-roles.yaml
+kustomize build overlays/runtimes | kubectl apply --server-side --force-conflicts -f -
+
+kubectl get deployment -n kubeflow-system kubeflow-trainer-controller-manager
+kubectl get pods -n kubeflow-system -l app.kubernetes.io/name=trainer
+kubectl get crd | grep -E 'trainer.kubeflow.org'
+kubectl get clustertrainingruntimes
+
+cd -
---
+++ tests/trainer_test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euxo pipefail
+KF_PROFILE=${1:-kubeflow-user-example-com}
+
+kubectl apply -f tests/trainer_job.yaml -n $KF_PROFILE
+
+kubectl get jobset pytorch-simple -n $KF_PROFILE
+kubectl get pods -n $KF_PROFILE --show-labels
+kubectl wait --for=condition=Ready pod -l batch.kubernetes.io/job-name=pytorch-simple-node-0 -n $KF_PROFILE --timeout=180s
+
+kubectl wait --for=condition=Complete job/pytorch-simple-node-0 -n $KF_PROFILE --timeout=450s
