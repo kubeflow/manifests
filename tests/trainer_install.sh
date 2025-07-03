9fd6196c686514033ba23acf546485a1730f5155
kunal-511 <yoyokvunal@gmail.com>
2025-07-03 14:52:16 +0530

    changed to overlays/kubeflow-platform
    Signed-off-by: kunal-511 <yoyokvunal@gmail.com>

--- tests/trainer_install.sh
+++ tests/trainer_install.sh
@@ -8,11 +8,8 @@ kustomize build base/crds | kubectl apply --server-side --force-conflicts -f -
 sleep 5
 kubectl wait --for condition=established crd/trainjobs.trainer.kubeflow.org --timeout=60s
 
-kustomize build overlays/manager | kubectl apply --server-side --force-conflicts -f -
+kustomize build overlays/kubeflow-platform | kubectl apply --server-side --force-conflicts -f -
 kubectl wait --for=condition=Available deployment/kubeflow-trainer-controller-manager -n kubeflow --timeout=180s
-
-kubectl apply --server-side --force-conflicts -f overlays/kubeflow-platform/kubeflow-trainer-roles.yaml
-kustomize build overlays/runtimes | kubectl apply --server-side --force-conflicts -f -
 
 kubectl get deployment -n kubeflow kubeflow-trainer-controller-manager
 kubectl get pods -n kubeflow -l app.kubernetes.io/name=trainer
