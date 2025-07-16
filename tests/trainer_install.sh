bde824e8aab7cd360c96c455bba5ebaf2adb55e7
kunal-511 <yoyokvunal@gmail.com>
2025-07-17 00:43:42 +0530

    changed to kubeflow-system namespace and add proper security policies
    Signed-off-by: kunal-511 <yoyokvunal@gmail.com>

--- tests/trainer_install.sh
+++ tests/trainer_install.sh
@@ -8,11 +8,25 @@ kustomize build base/crds | kubectl apply --server-side --force-conflicts -f -
 sleep 5
 kubectl wait --for condition=established crd/trainjobs.trainer.kubeflow.org --timeout=60s
 
-kustomize build overlays/kubeflow-platform | kubectl apply --server-side --force-conflicts -f -
-kubectl wait --for=condition=Available deployment/kubeflow-trainer-controller-manager -n kubeflow --timeout=180s
+kustomize build overlays/manager | kubectl apply --server-side --force-conflicts -f -
 
-kubectl get deployment -n kubeflow kubeflow-trainer-controller-manager
-kubectl get pods -n kubeflow -l app.kubernetes.io/name=trainer
+cd ../../../
+
+kustomize build common/kubeflow-system-namespace/base | kubectl apply -f -
+
+kustomize build common/networkpolicies/kubeflow-system | kubectl apply -f -
+
+cd applications/trainer/upstream
+
+kubectl wait --for=condition=Available deployment/kubeflow-trainer-controller-manager -n kubeflow-system --timeout=180s
+
+kustomize build overlays/runtimes | kubectl apply --server-side --force-conflicts -f -
+
+kubectl apply -f overlays/kubeflow-platform/kubeflow-trainer-roles.yaml
+
+
+kubectl get deployment -n kubeflow-system kubeflow-trainer-controller-manager
+kubectl get pods -n kubeflow-system -l app.kubernetes.io/name=trainer
 kubectl get crd | grep -E 'trainer.kubeflow.org'
 kubectl get clustertrainingruntimes
 
