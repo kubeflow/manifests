2178090b694e0d1ccb8eb91864b6b979c723c119
kunal-511 <yoyokvunal@gmail.com>
2025-07-17 10:54:58 +0530

    added wait for trainjob
    Signed-off-by: kunal-511 <yoyokvunal@gmail.com>

--- tests/trainer_test.sh
+++ tests/trainer_test.sh
@@ -4,6 +4,7 @@ KF_PROFILE=${1:-kubeflow-user-example-com}
 
 kubectl apply -f tests/trainer_job.yaml -n $KF_PROFILE
 
+kubectl wait --for=jsonpath='{.status.conditions[0].type}=Created' trainjob/pytorch-simple -n $KF_PROFILE --timeout=60s
 kubectl get jobset pytorch-simple -n $KF_PROFILE
 kubectl get pods -n $KF_PROFILE --show-labels
 kubectl wait --for=condition=Ready pod -l batch.kubernetes.io/job-name=pytorch-simple-node-0 -n $KF_PROFILE --timeout=180s
