ad02dcecaeee2a310319352d56ac4decc775a23f
kunal-511 <yoyokvunal@gmail.com>
2025-07-22 16:23:13 +0530

    added delay
    Signed-off-by: kunal-511 <yoyokvunal@gmail.com>

--- tests/trainer_test.sh
+++ tests/trainer_test.sh
@@ -4,6 +4,10 @@ KF_PROFILE=${1:-kubeflow-user-example-com}
 
 kubectl apply -f tests/trainer_job.yaml -n $KF_PROFILE
 
+sleep 10
+
+kubectl get trainjob pytorch-simple -n $KF_PROFILE -o yaml
+
 kubectl get jobset pytorch-simple -n $KF_PROFILE
 kubectl get pods -n $KF_PROFILE --show-labels
 kubectl wait --for=condition=Ready pod -l batch.kubernetes.io/job-name=pytorch-simple-node-0 -n $KF_PROFILE --timeout=180s
