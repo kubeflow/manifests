fb4e89623ffb17ec1fbaf6dcfaf0058475775424
Julius von Kohout <45896133+juliusvonkohout@users.noreply.github.com>
2025-08-08 10:26:23 +0200

    Update trainer_test.sh
    Signed-off-by: Julius von Kohout <45896133+juliusvonkohout@users.noreply.github.com>

--- tests/trainer_test.sh
+++ tests/trainer_test.sh
@@ -22,5 +22,5 @@ kubectl get pods -n kubeflow-system -l app.kubernetes.io/name=trainer
 kubectl get clustertrainingruntimes torch-distributed
 
 kubectl get pods -n $KF_PROFILE --show-labels
-kubectl wait --for=condition=Ready pod -l batch.kubernetes.io/job-name=pytorch-simple-node-0 -n $KF_PROFILE --timeout=180s
+kubectl wait --for=condition=Ready pod -l batch.kubernetes.io/job-name=pytorch-simple-node-0 -n $KF_PROFILE --timeout=60s
 kubectl wait --for=condition=Complete job/pytorch-simple-node-0 -n $KF_PROFILE --timeout=300s
