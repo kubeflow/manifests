1a4449bcf03cc24136fbc9aa21ba1e9745f6d624
kunal-511 <yoyokvunal@gmail.com>
2025-08-14 20:11:58 +0530

    fetch latest changes and update commit hash
    Signed-off-by: kunal-511 <yoyokvunal@gmail.com>

--- scripts/synchronize-trainer-manifests.sh
+++ scripts/synchronize-trainer-manifests.sh
@@ -9,7 +9,7 @@ setup_error_handling
 COMPONENT_NAME="trainer"
 REPOSITORY_NAME="kubeflow/trainer"
 REPOSITORY_URL="https://github.com/kubeflow/trainer.git"
-COMMIT="master"
+COMMIT="f12a6d3"
 REPOSITORY_DIRECTORY="trainer"
 SOURCE_DIRECTORY=${SOURCE_DIRECTORY:=/tmp/kubeflow-${COMPONENT_NAME}}
 BRANCH_NAME=${BRANCH_NAME:=synchronize-${COMPONENT_NAME}-manifests-${COMMIT?}}
