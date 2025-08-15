eed6788b7f0f1f9eef83cd43814f16e4df9ad4cc
Julius von Kohout <45896133+juliusvonkohout@users.noreply.github.com>
2025-08-15 13:33:59 +0200

    Update trivy_install.sh
    Signed-off-by: Julius von Kohout <45896133+juliusvonkohout@users.noreply.github.com>

--- tests/trivy_install.sh
+++ tests/trivy_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sudo sh -s -- -b /usr/local/bin v0.63.0
+curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sudo sh -s -- -b /usr/local/bin v0.65.0
