43966b55fd4d7571e12bd22b9781b09bb8628bee
kunal-511 <yoyokvunal@gmail.com>
2025-08-20 00:51:41 +0530

    reorder test in end to end test
    Signed-off-by: kunal-511 <yoyokvunal@gmail.com>

--- tests/training_operator_test.sh
+++ tests/training_operator_test.sh
@@ -12,4 +12,4 @@ kubectl get pods -n $KF_PROFILE --show-labels
 
 kubectl wait --for=condition=Ready pod -l training.kubeflow.org/replica-type=worker -n $KF_PROFILE --timeout=240s
 
-kubectl wait --for=condition=Succeeded pytorchjob/pytorch-simple -n $KF_PROFILE --timeout=7200s
+kubectl wait --for=condition=Succeeded pytorchjob/pytorch-simple -n $KF_PROFILE --timeout=600s
