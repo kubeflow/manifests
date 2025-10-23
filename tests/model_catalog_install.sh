31cd1d1ce753cb34e7c1cdd32c4e2f35977df2f1
kunal-511 <yoyokvunal@gmail.com>
2025-10-23 23:44:08 +0530

    Tests for Model Catalog
    Signed-off-by: kunal-511 <yoyokvunal@gmail.com>

---
+++ tests/model_catalog_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -euxo pipefail
+
+cd applications/model-registry/upstream/options/catalog && kustomize build . | kubectl apply -n kubeflow -f - && cd ../../../../../
+
+kubectl wait --for=condition=Available deployment/model-catalog-server -n kubeflow --timeout=120s
