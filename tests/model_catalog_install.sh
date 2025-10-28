1fb44bc7a923f79a685f7c668360565a2fe9ca3d
Kunal Dugar <yoyokvunal@gmail.com>
2025-10-28 15:37:18 +0530

    Update the test according to the v0.3.2 (#4)
    Signed-off-by: kunal-511 <yoyokvunal@gmail.com>
    Signed-off-by: tarilabs <matteo.mortari@gmail.com>

--- tests/model_catalog_install.sh
+++ tests/model_catalog_install.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 (
-    cd applications/model-registry/upstream/options/catalog
+    cd applications/model-registry/upstream/options/catalog/base
     kustomize build . | kubectl apply -n kubeflow -f -
 )
 
