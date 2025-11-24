d89364e37ee18fd5c9b97f20d6486dce228e04ee
juliusvonkohout <45896133+juliusvonkohout@users.noreply.github.com>
2025-11-24 16:22:49 +0100

    update script
    Signed-off-by: juliusvonkohout <45896133+juliusvonkohout@users.noreply.github.com>

--- scripts/synchronize-trainer-manifests.sh
+++ scripts/synchronize-trainer-manifests.sh
@@ -9,7 +9,7 @@ setup_error_handling
 COMPONENT_NAME="trainer"
 REPOSITORY_NAME="kubeflow/trainer"
 REPOSITORY_URL="https://github.com/kubeflow/trainer.git"
-COMMIT="2.1.0"
+COMMIT="v2.1.0"
 REPOSITORY_DIRECTORY="trainer"
 SOURCE_DIRECTORY=${SOURCE_DIRECTORY:=/tmp/kubeflow-${COMPONENT_NAME}}
 BRANCH_NAME=${BRANCH_NAME:=synchronize-${COMPONENT_NAME}-manifests-${COMMIT?}}
