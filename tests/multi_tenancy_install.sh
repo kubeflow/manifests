502ce6cae76e975424b676e6bfc5b5f33d53183d
danish9039 <danishsiddiqui040@gmail.com>
2026-02-15 23:02:49 +0530

    fix
    Signed-off-by: danish9039 <danishsiddiqui040@gmail.com>

--- tests/multi_tenancy_install.sh
+++ tests/multi_tenancy_install.sh
@@ -7,15 +7,8 @@ kubectl -n kubeflow wait --for=condition=Ready pods -l kustomize.component=profi
 
 echo "Installing Multitenancy Kubeflow Roles"
 kustomize build common/kubeflow-roles/base | kubectl apply -f -
-echo "Installing Namespaces"
-kustomize build common/namespaces/base | kubectl apply -f -
+
 
 echo "Installing Multitenancy Network policies"
 # Network policies are applied to existing namespaces
 kustomize build common/networkpolicies/base | kubectl apply -f -
-kustomize build common/cert-manager/overlays/kubeflow | kubectl apply -f -
-kustomize build common/dex/overlays/kubeflow | kubectl apply -f -
-kustomize build common/istio/istio-namespace/overlays/kubeflow | kubectl apply -f -
-kustomize build common/knative/knative-serving/overlays/kubeflow | kubectl apply -f -
-kustomize build common/kubeflow-system-namespace/base | kubectl apply -f -
-kustomize build common/oauth2-proxy/overlays/kubeflow | kubectl apply -f -
