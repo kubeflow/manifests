e1b620cf4d452753c695f53e5015e2b67992e51a
Julius von Kohout <45896133+juliusvonkohout@users.noreply.github.com>
2026-02-20 15:36:16 +0100

    Update cert-manager installation script to use base
    Signed-off-by: Julius von Kohout <45896133+juliusvonkohout@users.noreply.github.com>

--- tests/cert_manager_install.sh
+++ tests/cert_manager_install.sh
@@ -3,10 +3,10 @@ set -euxo pipefail
 echo "Installing cert-manager ..."
 cd common/cert-manager
 kubectl create namespace cert-manager || true
-kustomize build overlays/kubeflow | kubectl apply -f -
+kustomize build base | kubectl apply -f -
 echo "Waiting for cert-manager-webhook to be ready ..."
 kubectl wait --for=condition=Ready pod -l 'app=webhook' --timeout=180s -n cert-manager
-
+kustomize build overlays/kubeflow | kubectl apply -f -
 echo "Waiting for all cert-manager components to be ready ..."
 kubectl wait --for=condition=Ready pod -l 'app.kubernetes.io/instance=cert-manager' --timeout=180s -n cert-manager
 kubectl wait --for=jsonpath='{.subsets[0].addresses[0].targetRef.kind}'=Pod endpoints -l 'app in (cert-manager,webhook)' --timeout=180s -n cert-manager
