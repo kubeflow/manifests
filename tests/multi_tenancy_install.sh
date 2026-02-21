bb12332794f938eee1000723dbe0633f3eaca9d7
Julius von Kohout <45896133+juliusvonkohout@users.noreply.github.com>
2026-02-21 13:21:15 +0100

    Apply suggestion from @juliusvonkohout
    Signed-off-by: Julius von Kohout <45896133+juliusvonkohout@users.noreply.github.com>

--- tests/multi_tenancy_install.sh
+++ tests/multi_tenancy_install.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 echo "Installing Profiles Controller with PSS (Pod Security Standards)"
 kustomize build applications/profiles/pss | kubectl apply -f -
-kubectl -n kubeflow wait --for=condition=Available deployment/profiles-deployment --timeout 180s
+kubectl -n kubeflow wait --for=condition=Ready pods -l kustomize.component=profiles --timeout 180s
 
 echo "Installing Multitenancy Kubeflow Roles"
 kustomize build common/kubeflow-roles/base | kubectl apply -f -
