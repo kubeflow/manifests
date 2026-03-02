b578a72611e5b43eb93e80c69e1090bec33acb10
danish9039 <danishsiddiqui040@gmail.com>
2026-03-02 21:04:37 +0530

    dex: clean up split follow-up
    Signed-off-by: danish9039 <danishsiddiqui040@gmail.com>

--- scripts/synchronize-dex-manifests.sh
+++ scripts/synchronize-dex-manifests.sh
@@ -9,6 +9,7 @@ COMMIT="v2.45.0"
 BRANCH_NAME=${BRANCH_NAME:=synchronize-${COMPONENT_NAME}-manifests-${COMMIT?}}
 MANIFESTS_DIRECTORY=$(dirname $SCRIPT_DIRECTORY)
 DESTINATION_DIRECTORY=$MANIFESTS_DIRECTORY/common/${COMPONENT_NAME}
+UPSTREAM_DIRECTORY=$DESTINATION_DIRECTORY/base/upstream
 create_branch "$BRANCH_NAME"
 if [[ "$OSTYPE" == "darwin"* ]]; then
     sed -i "" "s|ghcr.io/dexidp/dex:v[0-9.]*|ghcr.io/dexidp/dex:${COMMIT}|g" \
@@ -17,8 +18,15 @@ else
     sed -i "s|ghcr.io/dexidp/dex:v[0-9.]*|ghcr.io/dexidp/dex:${COMMIT}|g" \
         $DESTINATION_DIRECTORY/base/deployment.yaml
 fi
+mkdir -p "$UPSTREAM_DIRECTORY"
+curl --silent --show-error --fail --location \
+    "https://raw.githubusercontent.com/${REPOSITORY_NAME}/${COMMIT}/scripts/manifests/crds/authcodes.yaml" \
+    > "$UPSTREAM_DIRECTORY/crds.yaml"
 SOURCE_TEXT="\[.*\](https://github.com/${REPOSITORY_NAME}/releases/tag/v.*)"
 DESTINATION_TEXT="\[${COMMIT#v}\](https://github.com/${REPOSITORY_NAME}/releases/tag/${COMMIT})"
 update_readme "$MANIFESTS_DIRECTORY" "$SOURCE_TEXT" "$DESTINATION_TEXT"
-commit_changes "$MANIFESTS_DIRECTORY" "Update ${REPOSITORY_NAME} manifests from ${COMMIT}" "$MANIFESTS_DIRECTORY"
+commit_changes "$MANIFESTS_DIRECTORY" "Update ${REPOSITORY_NAME} manifests from ${COMMIT}" \
+    "$DESTINATION_DIRECTORY/base/deployment.yaml" \
+    "$UPSTREAM_DIRECTORY/crds.yaml" \
+    "README.md"
 echo "Synchronization completed successfully."
