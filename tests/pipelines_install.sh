628270e6c14738fc987cb657b6901a6b7f883ff3
Julius von Kohout <45896133+juliusvonkohout@users.noreply.github.com>
2026-03-20 18:52:14 +0100

    Fix directory path for pipeline installation
    Signed-off-by: Julius von Kohout <45896133+juliusvonkohout@users.noreply.github.com>

--- tests/pipelines_install.sh
+++ tests/pipelines_install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 echo "Installing Pipelines ..."
-cd applications/pipeline/upstream
+cd applications/pipeline
 kubectl apply -f third-party/metacontroller/base/crd.yaml
 echo "Waiting for crd/compositecontrollers.metacontroller.k8s.io to be available ..."
 kubectl wait --for condition=established --timeout=30s crd/compositecontrollers.metacontroller.k8s.io
-kustomize build ../overlays | kubectl apply -f -
+kustomize build overlays | kubectl apply -f -
 sleep 60
 kubectl wait --for=condition=Ready pods --all --all-namespaces --timeout=600s \
   --field-selector=status.phase!=Succeeded
