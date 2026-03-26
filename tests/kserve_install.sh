79656c6cc41eb2857e38d4e0d09160d071089ec7
danish9039 <danishsiddiqui040@gmail.com>
2026-03-27 03:09:12 +0530

    align kserve downstream helpers
    Signed-off-by: danish9039 <danishsiddiqui040@gmail.com>

--- tests/kserve_install.sh
+++ tests/kserve_install.sh
@@ -2,16 +2,10 @@
 set -euxo pipefail
 echo "Installing Kserve ..."
 cd applications/kserve
-set +e
-for ((i=1; i<=3; i++)); do
-    if kustomize build kserve | kubectl apply --server-side --force-conflicts -f -; then
-        break
-    fi
-    kubectl wait --for=condition=Ready pods --all --all-namespaces --timeout=60s --field-selector=status.phase!=Succeeded
-    kubectl wait --for=condition=Ready certificate/serving-cert -n kubeflow --timeout=60s
-    kubectl get secret kserve-webhook-server-cert -n kubeflow -o name
-done
-set -e
+kustomize build kserve | kubectl apply --server-side --force-conflicts -f - || true
+kubectl wait --for=condition=Ready pods --all --all-namespaces --timeout=60s --field-selector=status.phase!=Succeeded
+kubectl wait --for=condition=Ready certificate/serving-cert -n kubeflow --timeout=60s
+kubectl wait --for=create secret/kserve-webhook-server-cert -n kubeflow --timeout=60s
 
 kubectl wait --for condition=established --timeout=30s crd/clusterservingruntimes.serving.kserve.io
 kustomize build kserve | kubectl apply --server-side --force-conflicts -f -
