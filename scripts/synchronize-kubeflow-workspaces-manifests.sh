b330d45869c43b3c015d9932252e24be5270c2af
Christian Heusel <christian@heusel.eu>
2026-09-07 15:46:56 +0200

    Update kubeflow/notebooks manifests to v2.0.0-beta.1 (#3598)
    Signed-off-by: Christian Heusel <christian@heusel.eu>

--- scripts/synchronize-kubeflow-workspaces-manifests.sh
+++ scripts/synchronize-kubeflow-workspaces-manifests.sh
@@ -6,7 +6,7 @@ setup_error_handling
 COMPONENT_NAME="workspaces"
 REPOSITORY_NAME="kubeflow/notebooks"
 REPOSITORY_URL="https://github.com/kubeflow/notebooks.git"
-COMMIT="v2.0.0-beta.0"
+COMMIT="v2.0.0-beta.1"
 REPOSITORY_DIRECTORY="$COMPONENT_NAME"
 SOURCE_DIRECTORY=${SOURCE_DIRECTORY:=/tmp/${COMPONENT_NAME}}
 BRANCH_NAME=${BRANCH_NAME:=synchronize-${COMPONENT_NAME}-manifests-${COMMIT?}}
